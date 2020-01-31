849a643c74c11dc71f2da0870e9d6e1c457bd9be
Adam Tizzone <ad-t@users.noreply.github.com>
2020-02-01 00:10:59 +1100

    migrated student library to query builder

--- backend/src/student.ts
+++ backend/src/student.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import {
-  Connection,
-  getConnection,
+  getRepository
 } from "typeorm";
 import { Job } from "./entity/job";
 import Helpers from "./helpers";
@@ -9,14 +8,12 @@ import Helpers from "./helpers";
 export default class StudentFunctions {
   public static async GetAllActiveJobs(_: Request, res: Response) {
     try {
-      const conn: Connection = await getConnection();
-      const jobs = await conn.getRepository(Job).find({
-        relations: ["company"],
-        where: {
-          approved: true,
-          hidden: false,
-        },
-      });
+      const jobs = await getRepository(Job)
+      .createQueryBuilder()
+      .leftJoinAndSelect("Job.company", "company")
+      .where("Job.approved = :approved", { approved: true })
+      .andWhere("Job.hidden = :hidden", { hidden: false })
+      .getMany();
 
       const fixedJobs = jobs.map((job) => { 
         const newJob: any = {};
@@ -36,14 +33,15 @@ export default class StudentFunctions {
 public static async GetJob(req: Request, res: Response) {
   try {
     Helpers.requireParameters(req.params.jobID);
-    const conn: Connection = await getConnection();
-    const jobInfo = await conn.getRepository(Job).findOneOrFail({
-      relations: ["company"],
-      where: {
-        approved: true,
-        id: parseInt(req.params.jobID, 10),
-      },
-    });
+    const jobInfo = await Helpers.doSuccessfullyOrFail(async () => {
+      return await getRepository(Job)
+      .createQueryBuilder()
+      .leftJoinAndSelect("Job.company", "company")
+      .where("Job.approved = :approved", { approved: true })
+      .andWhere("Job.id = :id", { id: parseInt(req.params.jobID, 10) })
+      .getOne();
+    }, `Couldn't find job with ID: ${req.params.jobID}`);
+
     res.send(jobInfo);
   } catch (error) {
     res.sendStatus(400);
