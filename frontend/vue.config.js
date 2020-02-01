8f9d2102c12941c04968b073cd28cb717f577a8e
Adam Tizzone <ad-t@users.noreply.github.com>
2020-02-01 13:20:36 +1100

    Fixed issue with sass-loader using deprecated data field

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/style/_variables.scss";`
+        prependData: `@import "@/style/_variables.scss";`
       }
     }
   }
