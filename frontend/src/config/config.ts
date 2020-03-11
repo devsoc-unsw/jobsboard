38394395028acd088c3d07284f01df6586ba29c0
Adam T <ad-t@users.noreply.github.com>
2020-03-11 21:59:36 +1100

    Frontend api root now changes depending on whether the node env is production or not

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,4 @@
 export default {
-  apiRoot: "https://www.jb.adamtiz.zone/api",
+  apiRoot: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8080" : "https://www.jb.adamtiz.zone/api",
   sessionStorageApiTokenKeyName: "jobs-board-api-token",
 };
