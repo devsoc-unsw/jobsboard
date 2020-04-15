cc025a59fcbccc12e2254255bd2c59e2aae144e1
Adam T <ad-t@users.noreply.github.com>
2020-04-16 00:45:48 +1000

    Temporarily restoring bootstrap for prod, but will be removed very soon as it is just there for deployment debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,6 +46,22 @@ app.use(helmet());
 // app.options("*", cors());
 if (process.env.NODE_ENV === "development") {
   app.use(cors());
+} else {
+  // assuming production, set up a particular config and allow only requests from
+  // the current URL to be consumed
+  const whitelist = [
+    'https://jobsboard.csesoc.unsw.edu.au'
+  ];
+  const corsOptions = {
+    origin: (origin: any, callback: Function) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }
+  app.use(cors(corsOptions));
 }
 
 const activeEntities = [
@@ -409,6 +425,9 @@ app.use(Middleware.genericLoggingMiddleware);
 
 app.listen(port, async () => {
   if (process.env.NODE_ENV === "development") {
+    await bootstrap();
+  } else {
+    // TODO(ad-t): This is very temporary for use in production, must remove very soon.
     await bootstrap();
   }
   MailFunctions.InitMailQueueScheduler(2000);
