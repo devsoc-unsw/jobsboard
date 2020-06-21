c7c1ddd17eb02aed71b826700831ca8a7fab2d25
Adam T <ad-t@users.noreply.github.com>
2020-06-21 14:35:59 +1000

    Local dev environment more portable with env files instead of docker secrets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -89,8 +89,8 @@ const specs = swaggerJsdoc(swaggerjsdocOptions);
 
 const options: ConnectionOptions = {
   type: "mysql",
-  host: "db",
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
