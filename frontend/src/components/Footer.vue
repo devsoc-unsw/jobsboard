6d15bd5741f6276eec59849c14a0b4882781276a
Adam T <ad-t@users.noreply.github.com>
2020-08-14 00:31:23 +1000

    Footer links now open in a new tab

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -7,18 +7,27 @@
           <font-awesome-icon icon="heart" />
         </span>
         by Adam
-        <a href="https://www.github.com/ad-t">
+        <a href="https://www.github.com/ad-t"i
+           target="_blank"
+           rel="noopener noreferrer"
+          >
           <span>
             <font-awesome-icon :icon="['fab', 'github']" />
           </span>
         </a>
-        <a href="https://www.linkedin.com/in/adam-tizzone">
+        <a href="https://www.linkedin.com/in/adam-tizzone"
+           target="_blank"
+           rel="noopener noreferrer"
+          >
           <span>
             <font-awesome-icon :icon="['fab', 'linkedin']" />
           </span>
         </a>
         on behalf of
-        <a href="https://www.csesoc.unsw.edu.au/teams/software-projects/">
+        <a href="https://www.csesoc.unsw.edu.au/teams/software-projects/"
+           target="_blank"
+           rel="noopener noreferrer"
+          >
           CSESoc Projects
         </a>
       </i>
