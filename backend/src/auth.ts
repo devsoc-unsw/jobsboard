a5e336bf3db8c36df036d388125322927dc392cd
Adam T <ad-t@users.noreply.github.com>
2020-09-08 00:27:29 +1000

    Beta testing now enabled through environment variable, added it to docker-compose as well

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -205,12 +205,13 @@ export default class Auth {
       "z5215628",
     ];
 
-    /*
-    if (!betaTesters.includes(zID)) {
-      Logger.Info(`REJECTED login from STUDENT=${zID} as they are not a beta tester.`);
-      return false;
+    if (process.env.RESTRICTED_LOGINS === "true") {
+      Logger.Info("Restricted logins ENABLED");
+      if (!betaTesters.includes(zID)) {
+        Logger.Info(`REJECTED login from STUDENT=${zID} as they are not a beta tester.`);
+        return false;
+      }
     }
-    */
 
     if (process.env.NODE_ENV !== "development") {
       if (/^[a-zA-Z0-9]+$/.test(zID)) {
