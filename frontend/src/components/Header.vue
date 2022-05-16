1f8c7035288fe6cd66e2fa065f09dd143a37dd7d
Darian Lee <darianlmj@gmail.com>
2022-05-16 15:53:28 +1000

    JOB-57 Made new header responsive and added dark mode icon

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="header-container">
     <img class="main-logo" :src="logo" alt="CSESoc" />
-    <div v-if="!loggedIn">
-      <button class="login-button" @click="toStudentLogin">Log In</button>
-    </div>
-    <div v-else-if="loggedIn">
-      <button class="logout-button" @click="logOut">Logout</button>
+    <div class="button-container">
+      <img class="moon-logo" :src="moon" alt="Toggle Theme" />
+      <div v-if="!loggedIn">
+        <button class="login-button" @click="toStudentLogin">Log In</button>
+      </div>
+      <div v-else-if="loggedIn">
+        <button class="logout-button" @click="logOut">Logout</button>
+      </div>
     </div>
   </div>
 </template>
@@ -13,12 +16,14 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import logo from "@/assets/logos/csesocwhite.png";
+import moon from "@/assets/moon.svg";
 
 export default Vue.extend({
   name: "Header",
   data() {
     return {
       logo: logo,
+      moon: moon,
     };
   },
   props: {
@@ -50,6 +55,42 @@ export default Vue.extend({
 
 .main-logo {
   width: 13%;
+
+  @media screen and (min-width: 600px) and (max-width: 899px) {
+    width: 20%;
+  }
+
+  @media screen and (min-width: 350px) and (max-width: 599px) {
+    width: 30%;
+  }
+
+  @media screen and (max-width: 349px) {
+    width: 40%;
+  }
+}
+
+.button-container {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.moon-logo {
+  margin-right: 19px;
+  transform: rotate(220deg);
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 599px) {
+    width: 20%;
+    margin-right: 9px;
+  }
+}
+
+.moon-logo path {
+  stroke: #000;
 }
 
 .login-button, 
@@ -68,6 +109,10 @@ export default Vue.extend({
     transform: translateY(-2px);
     box-shadow: -5px 5px 10px -1px rgb(0 0 0 / 15%);
     cursor: pointer;
+  }
+
+  @media screen and (max-width: 599px) {
+    font-size: 8pt;
   }
 }
 </style>
