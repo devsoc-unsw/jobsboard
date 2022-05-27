640416077668a417c383776b8059820c8bdf7a4d
Darian Lee <darianlmj@gmail.com>
2022-05-27 20:12:44 +1000

    Fixed type error when importing vue-quill-editor

--- frontend/src/components/RichTextEditor.vue
+++ frontend/src/components/RichTextEditor.vue
@@ -1,34 +1,33 @@
-
-<template> 
-    <div class="rich-text-editor">
-        <quill-editor 
-            ref="quillRichTextEditor"
-            v-model:content="description"
-            :value="description"
-            :options="editorOptions"
-        />
-        <h2> {{ description }} </h2>
-    </div>
+<template>
+  <div class="rich-text-editor">
+    <quill-editor
+      ref="quillRichTextEditor"
+      v-model:content="description"
+      :value="description"
+      :options="editorOptions"
+    />
+    <h2>{{ description }}</h2>
+  </div>
 </template>
 
-<script> 
+<script lang="ts">
 import { Vue } from "vue-property-decorator";
-import 'quill/dist/quill.snow.css'
-import { quillEditor } from 'vue-quill-editor';
+import "quill/dist/quill.snow.css";
+import quillEditor from "vue-quill-editor";
 
 export default Vue.extend({
-    components: {
-        quillEditor
-    }
+  components: {
+    quillEditor,
+  },
 });
 </script>
 
-<style> 
+<style>
 .ql-toolbar,
 .ql-editor {
-    background-color: white;
+  background-color: white;
 }
 .ql-editor {
-    height: 40vh;
+  height: 40vh;
 }
 </style>
