c9734b94921c200b279317f9b29b1523db4f8df3
Darian Lee <darianlmj@gmail.com>
2022-06-03 20:31:41 +1000

    Linting fixes for page not found

--- frontend/src/pages/PageNotFound.vue
+++ frontend/src/pages/PageNotFound.vue
@@ -1,13 +1,13 @@
 <template>
   <StudentViewTemplate notLoggedIn>
     <main class="flex flex-col items-center justify-center min-h-full w-full">
-      <img class="w-11/12 h-auto mt-8 mb-16 xs:w-1/2 sm:w-1/2" :src="Logo" alt="logo"/>
+      <img class="w-full h-auto mt-8 mb-16 xs:w-1/2 sm:w-1/2 md:w-2/5" :src="Logo" alt="logo"/>
       <h1 class="text-5xl font-bold text-jb-headings">Page not Found</h1>
       <p class="text-xl text-jb-subheadings my-10 mx-auto md:mx-[25%]">
-        Hmm... wondering how you ended up here in the middle of nowhere? Cause we're wondering that too. 
+        Hmm... wondering how you ended up here in the middle of nowhere? Cause we're wondering that too.
         But don't worry, hang tight and we will beem you back to the home page in a couple of seconds.
       </p>
-      <h3 class="text-xl">Redirecting</h3>
+      <h3 class="text-xl font-bold">Redirecting</h3>
       <div class="m-4">
         <div class="pulsing-dot">
           <div>
@@ -45,13 +45,13 @@ export default Vue.extend({
 <style lang="scss">
 .pulsing-dot {
   position: relative;
-  left: -9999px;
-  width: 10px;
-  height: 10px;
-  border-radius: 5px;
+  left: -624.938rem;
+  width: 0.625rem;
+  height: 0.625rem;
+  border-radius: 0.313rem;
   background-color: #2c8bf4;
   color: #2c8bf4;
-  box-shadow: 9999px 0 0 -5px #2c8bf4;
+  box-shadow: 624.938rem 0 0 -0.313rem #2c8bf4;
   animation: dotPulse 1.5s infinite linear;
   animation-delay: .25s;
 }
@@ -61,62 +61,61 @@ export default Vue.extend({
   display: inline-block;
   position: absolute;
   top: 0;
-  width: 10px;
-  height: 10px;
-  border-radius: 5px;
+  width: 0.625rem;
+  height: 0.625rem;
+  border-radius: 0.313rem;
   background-color: #2c8bf4;
   color: #2c8bf4;
 }
 
 .pulsing-dot::before {
-  box-shadow: 9984px 0 0 -5px #2c8bf4;
+  box-shadow: 624rem 0 0 -0.313rem #2c8bf4;
   animation: dotPulseBefore 1.5s infinite linear;
   animation-delay: 0s;
 }
 
 .pulsing-dot::after {
-  box-shadow: 10014px 0 0 -5px #2c8bf4;
+  box-shadow: 625.875rem 0 0 -0.313rem #2c8bf4;
   animation: dotPulseAfter 1.5s infinite linear;
   animation-delay: .5s;
 }
 
 @keyframes dotPulseBefore {
   0% {
-    box-shadow: 9980px 0 0 -5px #2c8bf4;
+    box-shadow: 623.75rem 0 0 -0.313rem #2c8bf4;
   }
   30% {
-    box-shadow: 9980px 0 0 2px #2c8bf4;
+    box-shadow: 623.75rem 0 0 0.125rem #2c8bf4;
   }
   60%,
   100% {
-    box-shadow: 9980px 0 0 -5px #2c8bf4;
+    box-shadow: 623.75rem 0 0 -0.313rem #2c8bf4;
   }
 }
 
 @keyframes dotPulse {
   0% {
-    box-shadow: 9999px 0 0 -5px #2c8bf4;
+    box-shadow: 624.938rem 0 0 -0.313rem #2c8bf4;
   }
   30% {
-    box-shadow: 9999px 0 0 2px #2c8bf4;
+    box-shadow: 624.938rem 0 0 0.125rem #2c8bf4;
   }
   60%,
   100% {
-    box-shadow: 9999px 0 0 -5px #2c8bf4;
+    box-shadow: 624.938rem 0 0 -0.313rem #2c8bf4;
   }
 }
 
 @keyframes dotPulseAfter {
   0% {
-    box-shadow: 10014px 0 0 -5px #2c8bf4;
+    box-shadow: 625.875rem 0 0 -0.313rem #2c8bf4;
   }
   30% {
-    box-shadow: 10014px 0 0 2px #2c8bf4;
+    box-shadow: 625.875rem 0 0 0.125rem #2c8bf4;
   }
   60%,
   100% {
-    box-shadow: 10014px 0 0 -5px #2c8bf4;
+    box-shadow: 625.875rem 0 0 -0.313rem #2c8bf4;
   }
 }
-
 </style>
