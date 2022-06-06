05a0c5f0c22b1a367a402f721dc61bd93d3eef24
Sunny <66097119+casio987@users.noreply.github.com>
2022-06-06 18:59:39 +1000

    Job 49 company add job page (#114)
    * add new columns to job entity
    
    * modify all routes that retrive jobs to now also return additional fields
    
    * add additional job entity columns
    
    * fix: update student demographic column type
    
    * update mock data and helper functions
    
    * fixed admin tests with old expiry date and write tests for createJob with new job columns
    
    * update payload and expiry timestamp for CreateJob route
    
    * write additional tests for company createjobs related to new job columns
    
    * remove redundant default values for job columns
    
    * remove resolved comments
    
    * update job field typing
    
    * implement new 'CompanyAddJob' page skeleton
    
    * implement new designs for 'CompanyAddJob' page
    
    * bind new input fields
    
    * update create job api call
    
    * update successful api call behaviour
    
    * add fair works link
    
    * remove input box shadows, increase page horizontal padding and implement responsiveness

--- frontend/src/pages/CompanyAddJob.vue
+++ frontend/src/pages/CompanyAddJob.vue
@@ -1,6 +1,7 @@
 <template>
   <LoggedInTemplate>
   <StudentViewTemplate loggedIn>
+  <!-- TODO: replace with tailwind modal -->
   <div class="modalWrapper">
     <Modal 
       v-if="modalVisible"
@@ -35,7 +36,17 @@
     </Modal>
   </div>
   <div class="contentBox">
-    <h1>Add a job</h1>
+    <h1>Post a Job</h1>
+    <p class="subheading">
+      Reach out to a talented pool of over 10 000 Computer Science and Engineering students
+    </p>
+    <!-- disclaimer box -->
+    <div class="disclaimerContainer">
+      Please ensure that you specify whether this is a paid position, and please understand that we will be cross checking this with the
+      <a href="https://www.fairwork.gov.au/starting-employment/unpaid-work/student-placements" target="_blank" rel="noopener noreferrer">Australian Fair Work Act 2009</a>
+      to determine whether the job post follows all guidelines and prioritises the safety of our members.
+    </div>
+    <!-- success/error popups -->
     <div v-if="success">
       <br/>
       <SuccessBox>
@@ -48,47 +59,151 @@
       {{ errorMsg }}
       </ErrorBox>
     </div>
-    <h2>Name of the role</h2>
+    <!-- input fields -->
+    <h2>Job Title</h2>
     <input 
       name="role"
       v-model="role"
       type="text"
-      placeholder="Role"
-      />
-    <h2>Job Description (Text only - for now!)</h2>
-    <h4>Please ensure that you specify whether this is a paid position, and please understand that we will be cross checking this with the <a href="https://www.fairwork.gov.au/pay/unpaid-work/student-placements">Australian Fair Work Act 2009</a> to determine whether the job post follows all guidelines and prioritises the safety of our members.</h4>
-    <textarea
+      placeholder="Job Title"
+    />
+    <h2>Job Description</h2>
+    <textarea 
       name="description"
       v-model="description"
       type="text"
-      placeholder="Job Description - Markdown headings and dot points are supported."
+      placeholder="Job Description - Markdown supported"
       rows="6"
-      />
+    />
     <h2>Application Link</h2>
     <input 
       name="applicationLink"
       v-model="applicationLink"
       type="text"
-      placeholder="Application Link"
-      />
-    <br />
-    <h2>Expiry Date</h2>
-    <input
-      type="date"
-      class="dateEntryBox"
-      v-model="selectedDate"
+      placeholder="www.example.com"
     />
-    <br />
-    <StandardButton>
-      <Button @callback="showJobModal">
-        Preview
-      </Button>
-    </StandardButton>
-    <GreenStandardButton>
-      <Button @callback="submitJobPost">
-        Post!
-      </Button>
-    </GreenStandardButton>
+    <div class="gridContainer">
+      <div class="columnFlex">
+        <h2>Application Expiry Date</h2>
+        <input
+          name="expiryDate"
+          v-model="expiryDate"
+          type="date"
+        />
+      </div>
+      <div class="columnFlex">
+        <h2>Is this position paid?</h2>
+        <select name="paidPosition" id="paidPosition" v-model="isPaidPosition">
+          <option value="" disabled selected>Please select an option</option>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+      </div>
+    </div>
+    <div class="gridContainer">
+      <div class="columnFlex">
+        <h2>Job Type</h2>
+        <select name="jobType" id="jobType" v-model="jobType">
+          <option value="" disabled selected>Please select an option</option>
+          <option value="intern">Intern</option>
+          <option value="grad">Grad</option>
+        </select>
+      </div>
+      <div class="columnFlex">
+        <h2>Job Mode</h2>
+        <select name="jobMode" id="jobMode" v-model="jobMode">
+          <option value="" disabled selected>Please select an option</option>
+          <option value="onsite">Onsite</option>
+          <option value="hybrid">Hybrid</option>
+          <option value="remote">Remote</option>
+        </select>
+      </div>
+    </div>
+    <h2>Applicant's Working Rights</h2>
+    <p class="subheading field">
+      Please check all that applies
+    </p>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="aus_ctz" id="wr_aus_ctz" v-model="workingRights" />
+      <label for="wr_aus_ctz">Australian Citizen</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="aus_perm_res" id="wr_aus_perm_res" v-model="workingRights" />
+      <label for="wr_aus_perm_res">Australian Permanent Resident</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="aus_stud_visa" id="wr_aus_stud_visa" v-model="workingRights" />
+      <label for="wr_aus_stud_visa">Australian Student Visa</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="aus_temp_grad_visa" id="wr_aus_temp_grad_visa" v-model="workingRights" />
+      <label for="wr_aus_temp_grad_visa">Australian Temporary Grad Visa</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="nz_ctz_and_perm_res" id="wr_nz_ctz_and_perm_res" v-model="workingRights" />
+      <label for="wr_nz_ctz_and_perm_res">NZ Citizen/Permanent Resident</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="no_wr" id="wr_no_wr" v-model="workingRights" />
+      <label for="wr_no_wr">No Working Rights</label>
+    </div>
+    <div class="inputLabelContainer">
+      <input type="checkbox" value="all" id="wr_all" v-model="workingRights" />
+      <label for="wr_all">All</label>
+    </div>
+    <div class="gridContainer">
+      <div class="columnFlex">
+        <h2>Who Should Apply for this Role?</h2>
+        <p class="subheading field">
+          Please check all that applies
+        </p>
+        <div>
+          <div class="inputLabelContainer">
+            <input type="checkbox" value="final_year" id="student_demographic_final_year" v-model="studentDemographic" />
+            <label for="student_demographic_final_year">Graduates</label>
+          </div>
+          <div class="inputLabelContainer">
+            <input type="checkbox" value="penultimate" id="student_demographic_penultimate" v-model="studentDemographic" />
+            <label for="student_demographic_penultimate">Penultimate Students</label>
+          </div>
+          <div class="inputLabelContainer">
+            <input type="checkbox" value="all" id="student_demographic_all" v-model="studentDemographic" />
+            <label for="student_demographic_all">All Students</label>
+          </div>
+        </div>
+      </div>
+      <div class="columnFlex">
+        <h2>Applicant's WAM</h2>
+        <div>
+          <div class="inputLabelContainer">
+            <input type="radio" value="HD" id="applicantWam_HD" v-model="wamRequirements" />
+            <label for="applicantWam_HD">High Distinction | 85 and above</label>
+          </div>
+          <div class="inputLabelContainer">
+            <input type="radio" value="D" id="applicantWam_D" v-model="wamRequirements" />
+            <label for="applicantWam_D">Distinction | 75 and above</label>
+          </div>
+          <div class="inputLabelContainer">
+            <input type="radio" value="C" id="applicantWam_C" v-model="wamRequirements" />
+            <label for="applicantWam_C">Credit | 65 and above</label>
+          </div>
+          <div class="inputLabelContainer">
+            <input type="radio" value="none" id="applicantWam_none" v-model="wamRequirements" />
+            <label for="applicantWam_none">No preference</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2>Additional Information</h2>
+    <textarea 
+      name="additionalInfo"
+      v-model="additionalInfo"
+      type="text"
+      placeholder="Please note down any additional information that will make recommending jobs to students easier."
+      rows="6"
+    />
+    <button @click="showJobModal" id="preview_button">Preview</button>
+    <button @click="submitJobPost" id="submit_button">Post Job</button>
   </div>
   </StudentViewTemplate>
   </LoggedInTemplate>
@@ -103,10 +218,6 @@ import StudentViewTemplate from "@/components/StudentViewTemplate.vue";
 import ErrorBox from "@/components/ErrorBox.vue";
 import SuccessBox from "@/components/SuccessBox.vue";
 import LoggedInTemplate from "@/components/LoggedInTemplate.vue";
-import BackButton from "@/components/buttons/back.vue";
-import Button from "@/components/buttons/button.vue";
-import StandardButton from "@/components/buttons/StandardButton.vue";
-import GreenStandardButton from "@/components/buttons/GreenStandardButton.vue";
 import Modal from "@/components/Modal.vue";
 import JobDescriptionView from "@/components/JobDescriptionView.vue";
 
@@ -120,10 +231,6 @@ export default Vue.extend({
     SuccessBox,
     ErrorBox,
     LoggedInTemplate,
-    BackButton,
-    Button,
-    StandardButton,
-    GreenStandardButton,
     Modal,
     JobDescriptionView,
   },
@@ -132,6 +239,14 @@ export default Vue.extend({
       role: "",
       description: "",
       applicationLink: "",
+      expiryDate: "",
+      isPaidPosition: "",
+      jobType: "",
+      jobMode: "",
+      workingRights: [],
+      studentDemographic: [],
+      wamRequirements: "",
+      additionalInfo: "",
       error: false,
       errorMsg: "",
       success: false,
@@ -139,13 +254,12 @@ export default Vue.extend({
       apiToken: this.$store.getters.getApiToken,
       modalVisible: false,
       modalContent: "",
-      selectedDate: "",
     };
   },
   methods: {
     async submitJobPost() {
       // create a date object using this value
-      let jobDate = new Date(this.selectedDate);
+      let jobDate = new Date(this.expiryDate);
       // set to the end of the set day
       jobDate.setHours(23);
       jobDate.setMinutes(59);
@@ -161,6 +275,13 @@ export default Vue.extend({
           description: this.description,
           applicationLink: this.applicationLink,
           expiry: jobDate.valueOf(),
+          jobMode: this.jobMode,
+          studentDemographic: this.studentDemographic,
+          jobType: this.jobType,
+          workingRights: this.workingRights,
+          wamRequirements: this.wamRequirements,
+          additionalInfo: this.additionalInfo,
+          isPaid: this.isPaidPosition,
         }),
       });
 
@@ -169,6 +290,10 @@ export default Vue.extend({
         this.$store.dispatch("setApiToken", msg.token);
         this.success = true;
         this.successMsg = "Job posted! This job will be made available to students shortly. Redirecting to your dashboard...";
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        })
         setTimeout(() => {
           this.$router.push("/company/home");
         }, 5000);
@@ -200,11 +325,10 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-.bigTextEntry {
-  width: 100%;
-  min-height: 100%;
-}
 .contentBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 80%;
   margin-left: auto;
   margin-right: auto;
@@ -219,20 +343,108 @@ export default Vue.extend({
 .modalGroup {
   padding: 0.5rem;
 }
-input, textarea {
+input, textarea, select {
+  -ms-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  -webkit-box-sizing: content-box; 
+  box-sizing: content-box;
   font-family: 'sans-serif';
+  border: 1px solid black;
+  border-left-style: solid;
+  border-left-width: 5px;
+  border-left-color: $blue;
+  border-radius: 0.25rem;
+  align-self: flex-start;
+  margin: 0;
+  width: 100%;
+  box-shadow: none;
+  resize: none;
 }
-@media screen and (min-width: 900px) {
+input[type="checkbox"], input[type="radio"] {
+  align-self: center;
+  margin-right: 0.5rem;
+  width: auto;
+}
+h2 {
+  margin-bottom: 0;
+  font-weight: bold;
+  align-self: flex-start;
+}
+h1 {
+  margin-bottom: 0;
+  font-weight: bold;
+}
+.subheading {
+  margin: 0 0 3.125rem 0;
+  color: #00000080;
+}
+.field {
+  align-self: flex-start;
+  margin-bottom: 0.5rem;
+}
+.disclaimerContainer {
+  border: 1px solid black;
+  background-color: #FFFAE7;
+  text-align: center;
+  padding: 0.75rem;
+}
+.disclaimerContainer > a {
+  color: #2C8BF4;
+}
+.gridContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  text-align: left;
+}
+.columnFlex {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: left;
+  width: 45%;
+}
+.inputLabelContainer {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-self: flex-start;
+  text-align: left;
+}
+#preview_button {
+  border: none;
+  color: #2C8BF4;
+  background-color: white;
+  font-size: 1rem;
+  margin-top: 1.5rem;
+  cursor: pointer
+}
+#submit_button {
+  border-radius: 0.5rem;
+  border: 2px solid #4E5F78;
+  font-size: 1rem;
+  padding: 0.5rem 2rem;
+  margin-top: 1.5rem;
+  cursor: pointer;
+}
+@media screen and (max-width: 1030px) {
   input, textarea {
     padding: 1rem;
     background: $white;
   }
   .contentBox {
     width: 85%;
+    // text-align: left;
   }
-}
-.dateEntryBox {
-  width: auto;
-  font-family: 'sans-serif';
+  .gridContainer {
+    flex-direction: column;
+  }
+  .columnFlex {
+    width: 100%;
+  }
+  h2 {
+    align-self: center;
+  }
 }
 </style>
