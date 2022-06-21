0bf52b16579220b7f06e9e89796ff668d0f3a68e
samyu1204 <87253028+samyu1204@users.noreply.github.com>
2022-06-22 02:11:02 +1000

    Progress on company home page

--- frontend/src/pages/CompanyAccountHome.vue
+++ frontend/src/pages/CompanyAccountHome.vue
@@ -1,35 +1,37 @@
 <template>
   <LoggedInTemplate>
   <StudentViewTemplate>
-    <div class="px-[20%]">
+    <div class="px-[10%]">
       <h1 class="font-bold text-5xl text-[#1a324e] text-left leading-[72px] drop-shadow-md m-0">Welcome Back!👋</h1>
       <p class="text-xl text-jb-subheadings my-4 text-left">Accelerate your search for talented job applicants today with us!</p>
       <h1 class="font-bold text-4xl text-[#1a324e] text-center leading-[72px]">Board</h1>
       <p class="text-xl text-jb-subheadings my-4 text-left">Add a new job post with our “Post Job” profile card to your board 
         or manage your existing jobs by clicking on the profile card of any active jobs listed.</p>
       
-      <!-- Board select dropdown -->
-      <div class="text-left ml-4 flex mt-10">
-        <div>
-          <font-awesome-icon icon="bars" class="text-2xl" />
+      <div class="w-[700px] m-auto mt-8">
+        <!-- Board select dropdown -->
+        <div class="text-left flex ml-2">
+          <div>
+            <font-awesome-icon icon="bars" class="text-2xl" />
+          </div>
+          <div>
+            <select name="boards" id="board" class="bg-[#F6F9FC] ml-4 font-bold text-[#1A5D89] text-lg">
+              <option value="posted_jobs">Posted Jobs</option>
+              <option value="expired_jobs ">Expired Jobs</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <select name="boards" id="board" class="bg-[#F6F9FC] ml-4 font-bold text-[#1A5D89] text-lg">
-            <option value="posted_jobs">Posted Jobs</option>
-            <option value="expired_jobs ">Expired Jobs</option>
-          </select>
+        
+        <!-- Board -->
+        <div class="w-[700px] bg-[#75B2F5] h-[600px] m-auto rounded-xl grid place-items-center mt-4">
+          <div class="w-[98%] bg-[#ffffff] h-[588px] m-auto rounded-xl grid grid-cols-3 overflow-y-scroll">
+            <JobProfileCard  />
+            <JobProfileCard  />
+
+            <!-- v-for="member in members" :member="member" :key="member.name" -->
+          </div>
         </div>
       </div>
-
-
-
-      <!-- Board -->
-      <div class="w-[10/12] bg-[#75B2F5] h-[600px] m-auto rounded-xl grid place-items-center mt-4">
-        <div class="w-[98%] bg-[#ffffff] h-[588px] m-auto rounded-xl">
-
-        </div>
-      </div>
-
     </div>
   </StudentViewTemplate>
   </LoggedInTemplate>
@@ -41,6 +43,7 @@ import StudentViewTemplate from "@/components/StudentViewTemplate.vue";
 import LoggedInTemplate from "@/components/LoggedInTemplate.vue";
 import Button from "@/components/buttons/button.vue";
 import StandardButton from "@/components/buttons/StandardButton.vue";
+import JobProfileCard from "@/components/JobProfileCard.vue"
 
 export default Vue.extend({
   name: "CompanyAccountHome",
@@ -49,6 +52,7 @@ export default Vue.extend({
     LoggedInTemplate,
     Button,
     StandardButton,
+    JobProfileCard,
   },
   methods: {
     goToCompanyJobAdd() {
@@ -62,433 +66,26 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-.buttonBox {
-  padding: 2%;
+::-webkit-scrollbar {
+  width: 18px;    
 }
 
-.button {
-  min-width: 70%;
-  max-width: 70%;
-  border-radius: 0.5rem;
-  /* padding-top: 2%; */
-  /* padding-bottom: 2%; */
-  /* padding-left: 5%; */
-  /* padding-right: 5%; */
-  /* margin: 1%; */
+::-webkit-scrollbar-track {
+  background-color: white;
+  border-radius: 15px;
 }
 
-.editButton {
-  background: $white;
-  color: $blue;
+::-webkit-scrollbar-button {
+  height: 40px; //for vertical scrollbar
 }
 
-.postButton {
-  border: 1px solid $blue;
-  background: $blue;
-  color: $white;
-}
-* {
-  user-select: none;
-}
-
-*:focus {
-  outline: none;
-}
-
-html,
-body {
-  height: 100%;
-  min-height: 100%;
-}
-
-body {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  padding-bottom: 100px;
-  margin: 0;
-  background-color: #ebf3f3;
-}
-
-.brd {
-  border: 1px solid #e2eded;
-  border-color: #eaf1f1 #e4eded #dbe7e7 #e4eded;
-}
-
-#info {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  color: #2d3667;
-  font-size: 16px;
-  text-align: center;
-  padding: 14px;
-  background-color: #f3f9f9;
-}
-
-#app-cover {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  width: 300px;
-  height: 42px;
-  margin: 100px auto 0 auto;
-  z-index: 1;
-}
-
-#select-button {
-  position: relative;
-  height: 16px;
-  padding: 12px 14px;
-  background-color: #fff;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-#options-view-button {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  opacity: 0;
-  cursor: pointer;
-  z-index: 3;
-}
-
-#selected-value {
-  font-size: 16px;
-  line-height: 1;
-  margin-right: 26px;
-}
-
-.option i {
-  width: 16px;
-  height: 16px;
-}
-
-.option,
-.label {
-  color: #2d3667;
-  font-size: 16px;
-}
-
-#chevrons {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 12px;
-  padding: 9px 14px;
-}
-
-#chevrons i {
-  display: block;
-  height: 50%;
-  color: #d1dede;
-  font-size: 12px;
-  text-align: right;
-}
-
-#options-view-button:checked + #select-button #chevrons i {
-  color: #2d3667;
-}
-
-.options {
-  position: absolute;
-  left: 0;
-  width: 250px;
-}
-
-#options {
-  position: absolute;
-  top: 42px;
-  right: 0;
-  left: 0;
-  width: 298px;
-  margin: 0 auto;
-  background-color: #fff;
-  border-radius: 4px;
-}
-
-#options-view-button:checked ~ #options {
-  border: 1px solid #e2eded;
-  border-color: #eaf1f1 #e4eded #dbe7e7 #e4eded;
-}
-
-.option {
-  position: relative;
-  line-height: 1;
-  transition: 0.3s ease all;
-  z-index: 2;
-}
-
-.option i {
-  position: absolute;
-  left: 14px;
-  padding: 0;
-  display: none;
-}
-
-#options-view-button:checked ~ #options .option i {
-  display: block;
-  padding: 12px 0;
-}
-
-.label {
-  display: none;
-  padding: 0;
-  margin-left: 27px;
-}
-
-#options-view-button:checked ~ #options .label {
-  display: block;
-  padding: 12px 14px;
-}
-
-.s-c {
-  position: absolute;
-  left: 0;
-  width: 100%;
-  height: 50%;
-}
-
-.s-c.top {
-  top: 0;
-}
-
-.s-c.bottom {
-  bottom: 0;
-}
-
-input[type="radio"] {
-  position: absolute;
-  right: 0;
-  left: 0;
-  width: 100%;
-  height: 50%;
-  margin: 0;
-  opacity: 0;
-  cursor: pointer;
-}
-
-.s-c:hover ~ i {
-  color: #fff;
-  opacity: 0;
-}
-
-.s-c:hover {
-  height: 100%;
-  z-index: 1;
-}
-
-.s-c.bottom:hover + i {
-  bottom: -25px;
-  animation: moveup 0.3s ease 0.1s forwards;
-}
-
-.s-c.top:hover ~ i {
-  top: -25px;
-  animation: movedown 0.3s ease 0.1s forwards;
-}
-
-@keyframes moveup {
-  0% {
-    bottom: -25px;
-    opacity: 0;
-  }
-  100% {
-    bottom: 0;
-    opacity: 1;
-  }
-}
-
-@keyframes movedown {
-  0% {
-    top: -25px;
-    opacity: 0;
-  }
-  100% {
-    top: 0;
-    opacity: 1;
-  }
-}
-
-.label {
-  transition: 0.3s ease all;
-}
-
-.opt-val {
-  position: absolute;
-  left: 14px;
-  width: 217px;
-  height: 21px;
-  opacity: 0;
-  background-color: #fff;
-  transform: scale(0);
-}
-
-.option input[type="radio"]:checked ~ .opt-val {
-  opacity: 1;
-  transform: scale(1);
-}
-
-.option input[type="radio"]:checked ~ i {
-  top: 0;
-  bottom: auto;
-  opacity: 1;
-  animation: unset;
-}
-
-.option input[type="radio"]:checked ~ i,
-.option input[type="radio"]:checked ~ .label {
-  color: #fff;
-}
-
-.option input[type="radio"]:checked ~ .label:before {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
-}
-
-#options-view-button:not(:checked)
-  ~ #options
-  .option
-  input[type="radio"]:checked
-  ~ .opt-val {
-  top: -30px;
-}
-
-.option:nth-child(1) input[type="radio"]:checked ~ .label:before {
-  background-color: #000;
-  border-radius: 4px 4px 0 0;
-}
-
-.option:nth-child(1) input[type="radio"]:checked ~ .opt-val {
-  top: -31px;
-}
-
-.option:nth-child(2) input[type="radio"]:checked ~ .label:before {
-  background-color: #ea4c89;
-}
-
-.option:nth-child(2) input[type="radio"]:checked ~ .opt-val {
-  top: -71px;
-}
-
-.option:nth-child(3) input[type="radio"]:checked ~ .label:before {
-  background-color: #0057ff;
-}
-
-.option:nth-child(3) input[type="radio"]:checked ~ .opt-val {
-  top: -111px;
-}
-
-.option:nth-child(4) input[type="radio"]:checked ~ .label:before {
-  background-color: #32c766;
-}
-
-.option:nth-child(4) input[type="radio"]:checked ~ .opt-val {
-  top: -151px;
-}
-
-.option:nth-child(5) input[type="radio"]:checked ~ .label:before {
-  background-color: #f48024;
-}
-
-.option:nth-child(5) input[type="radio"]:checked ~ .opt-val {
-  top: -191px;
-}
-
-.option:nth-child(6) input[type="radio"]:checked ~ .label:before {
-  background-color: #006400;
-  border-radius: 0 0 4px 4px;
-}
-
-.option:nth-child(6) input[type="radio"]:checked ~ .opt-val {
-  top: -231px;
-}
-
-.option .fa-codepen {
-  color: #000;
-}
-
-.option .fa-dribbble {
-  color: #ea4c89;
-}
-
-.option .fa-behance {
-  color: #0057ff;
-}
-
-.option .fa-hackerrank {
-  color: #32c766;
-}
-
-.option .fa-stack-overflow {
-  color: #f48024;
-}
-
-.option .fa-free-code-camp {
-  color: #006400;
-}
-
-#option-bg {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  height: 40px;
-  transition: 0.3s ease all;
-  z-index: 1;
-  display: none;
-}
-
-#options-view-button:checked ~ #options #option-bg {
-  display: block;
-}
-
-.option:hover .label {
-  color: #fff;
-}
-
-.option:nth-child(1):hover ~ #option-bg {
-  top: 0;
-  background-color: #000;
-  border-radius: 4px 4px 0 0;
-}
-
-.option:nth-child(2):hover ~ #option-bg {
-  top: 40px;
-  background-color: #ea4c89;
-}
-
-.option:nth-child(3):hover ~ #option-bg {
-  top: 80px;
-  background-color: #0057ff;
-}
-
-.option:nth-child(4):hover ~ #option-bg {
-  top: 120px;
-  background-color: #32c766;
-}
-
-.option:nth-child(5):hover ~ #option-bg {
-  top: 160px;
-  background-color: #f48024;
-}
 
-.option:nth-child(6):hover ~ #option-bg {
-  top: 200px;
-  background-color: #006400;
-  border-radius: 0 0 4px 4px;
+::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: white;
+    border-radius: 15px;    
+    box-shadow: inset 0 0 10px 10px #E0DFDF;
+    border: solid 6px transparent;
 }
 
 </style>
