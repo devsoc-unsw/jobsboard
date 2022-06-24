a63573963aa3d4d11b0baf075ab8063c854d726a
sunny_thinkpad <sunnyw987@gmail.com>
2022-06-24 21:50:07 +1000

    adjust spacing for alert component

--- frontend/src/components/Alert.vue
+++ frontend/src/components/Alert.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex p-4 mb-4 rounded-lg" :class="getContainerStyles" role="alert" v-if="isOpen">
+  <div class="flex p-4 mb-8 mt-2 rounded-lg" :class="getContainerStyles" role="alert" v-if="isOpen">
     <svg class="flex-shrink-0 w-5 h-5" :class="getTextStyles" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
     <div class="ml-3 text-sm font-medium" :class="getTextStyles">
       {{ alertMsg }}
     </div>
-    <button @click="handleClose" type="button" class="ml-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8" :class="getButtonStyles" aria-label="Close">
+    <button @click="handleClose" type="button" class="ml-2 -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8" :class="getButtonStyles" aria-label="Close">
       <span class="sr-only">Close</span>
       <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
