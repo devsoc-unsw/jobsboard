474337f28063c93c496758c47a3270ffbf8d733e
joanna209 <joanna.he3@gmail.com>
2022-06-25 17:33:29 +1000

    fix(frontend): zid and password input spacing

--- frontend/src/pages/StudentLoginPage.vue
+++ frontend/src/pages/StudentLoginPage.vue
@@ -1,8 +1,9 @@
 <template>
   <StudentViewTemplate notLoggedIn>
     <div class="h-full flex flex-col justify-center items-center py-16">
-      <h1 class="font-bold text-3xl text-jb-headings pb-4">Student Login</h1>
-      <p class="text-lg text-jb-subheadings pb-4">
+      <h1 class="font-bold text-3xl text-jb-headings">Student Login</h1>
+      <br/>
+      <p class="text-lg text-jb-subheadings">
         Please enter your zID in the format zXXXXXXX and your zPass.
       </p>
       <div v-if="error">
@@ -12,18 +13,19 @@
         </ErrorBox>
       </div>
       <br/>
+      <br/>
       <div class="text-left">
-        <label for="zID" class="font-bold text-lg text-jb-subheadings px-3">zID {""}</label>
-        <input type="text" placeholder="zXXXXXXX" v-model="zID" class="peer border-l-4 p-4 border-jb-textlink rounded-md shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full" @keyup.enter="performLogin()" required>
+        <label for="zID" class="font-bold text-lg text-jb-subheadings pl-2">zID &nbsp;&nbsp; </label>
+        <input type="text" placeholder="zXXXXXXX" v-model="zID" class="peer border-l-4 p-4 border-jb-textlink rounded-md shadow-btn w-full text-lg focus:outline-jb-textlink" @keyup.enter="performLogin()" required>
         <p class="invisible peer-invalid:visible text-jb-warning text-sm font-bold text-left pl-2 pt-3">Please provide a valid zID. </p>
       </div>
       <div class="text-left">
-        <label for="password" class="font-bold text-lg text-jb-subheadings">Password</label>
-        <input type="password" placeholder="zPass" v-model="password" class="peer border-l-4 p-4 border-jb-textlink rounded-md shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full" @keyup.enter="performLogin()" required>
-        <p class="invisible peer-invalid:visible text-jb-warning text-sm font-bold text-left pl-2 pt-3">Please provide a valid password.</p>
+        <label for="password" class="font-bold text-lg text-jb-subheadings pl-2">zPass</label>
+        <input type="password" placeholder="zPass" v-model="password" class="peer border-l-4 p-4 border-jb-textlink rounded-md shadow-btn w-full text-lg focus:outline-jb-textlink" @keyup.enter="performLogin()" required>
+        <p class="invisible peer-invalid:visible text-jb-warning text-sm font-bold text-left pl-2 pt-3">Please provide a valid zPass.</p>
       </div>
       <br/>
-      <p class="text-lg text-jb-subheadings text-center py-4">
+      <p class="text-lg text-jb-subheadings text-center">
           Not a student? <span 
           class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
                       cursor-pointer hover:text-jb-textlink-hovered"
@@ -32,6 +34,7 @@
           Company Login
           </span>
       </p>
+      <br/>
       <button type="submit" class="bg-jb-textlink rounded-md w-40 h-11 p-2 text-white font-bold text-base border-0 shadow-btn duration-200 ease-linear cursor-pointer hover:bg-jb-btn-hovered hover:shadow-btn-hovered">
           <span class="p-2 text-white" @click="performLogin()">Log In</span>
       </button>
