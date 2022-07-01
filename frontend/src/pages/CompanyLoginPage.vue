73d6c3eddb5ec7afc8dfd6a850e2aff2aa9e5505
joanna209 <joanna.he3@gmail.com>
2022-07-01 18:13:52 +1000

    refactor(frontend): code style

--- frontend/src/pages/CompanyLoginPage.vue
+++ frontend/src/pages/CompanyLoginPage.vue
@@ -9,62 +9,88 @@
       <!-- Error Alert -->
       <Alert 
         alertType="error" 
-        alertMsg="Invalid credentials. Please try again." 
+        alertMsg="Invalid credentials. Please try again."
         :isOpen="isAlertOpen"
-        :handleClose="() => { this.isAlertOpen = false }" 
+        :handleClose="() => { this.isAlertOpen = false }"
       />
 
       <!-- Email Input -->
       <div class="w-4/5 relative group mt-4 mb-6 sm:w-1/2 md:w-2/5 xl:w-1/4">
-        <input name="email" id="email" v-model="username" type="text"
+        <input 
+          name="email"
+          id="email"
+          v-model="username"
+          type="text"
           class="font-bold border-l-4 border-jb-textlink rounded-md p-4 shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full peer"
-          @keyup.enter="performCompanyLogin()" required />
-        <label for="email"
+          @keyup.enter="performCompanyLogin()"
+          required 
+        />
+        <label
+          for="email"
           class="transform transition-all duration-400 absolute top-7 left-0 h-full flex items-center font-bold text-lg text-jb-placeholder/60 pl-6 pb-[3.75rem]
                  group-focus-within:text-base group-focus-within:h-1/2 group-focus-within:-translate-y-full
                  group-focus-within:pl-2 group-focus-within:pb-10 group-focus-within:text-jb-textlink
-                 peer-valid:text-base peer-valid:h-1/2 peer-valid:-translate-y-full peer-valid:pl-2 peer-valid:pb-10 peer-valid:text-jb-textlink">
+                 peer-valid:text-base peer-valid:h-1/2 peer-valid:-translate-y-full peer-valid:pl-2 peer-valid:pb-10 peer-valid:text-jb-textlink"
+        >
           Email
         </label>
       </div>
 
       <!-- Password Input -->
       <div class="w-4/5 relative group mt-4 mb-6 sm:w-1/2 md:w-2/5 xl:w-1/4">
-        <input name="password" id="password" v-model="password" type="password"
+        <input 
+          name="password" 
+          id="password" 
+          v-model="password" 
+          type="password"
           class="font-bold border-l-4 border-jb-textlink rounded-md p-4 shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full peer"
-          @keyup.enter="performCompanyLogin()" required />
-        <label for="password"
+          @keyup.enter="performCompanyLogin()" 
+          required 
+        />
+        <label 
+          for="password"
           class="transform transition-all duration-400 absolute top-7 left-0 h-full flex items-center font-bold text-lg text-jb-placeholder/60 pl-6 pb-[3.75rem]
                  group-focus-within:text-base group-focus-within:h-1/2 group-focus-within:-translate-y-full
                  group-focus-within:pl-2 group-focus-within:pb-10 group-focus-within:text-jb-textlink
-                 peer-valid:text-base peer-valid:h-1/2 peer-valid:-translate-y-full peer-valid:pl-2 peer-valid:pb-10 peer-valid:text-jb-textlink">
+                 peer-valid:text-base peer-valid:h-1/2 peer-valid:-translate-y-full peer-valid:pl-2 peer-valid:pb-10 peer-valid:text-jb-textlink"
+        >
           Password
         </label>
       </div>
 
       <p class="text-lg text-jb-subheadings text-center my-2">
         Not a company?
-        <router-link class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
-                        cursor-pointer hover:text-jb-textlink-hovered" to="/login/student">
+        <router-link 
+          class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
+                        cursor-pointer hover:text-jb-textlink-hovered" 
+          to="/login/student"
+        >
           Student Login
         </router-link>
       </p>
       <p class="text-lg text-jb-subheadings text-center my-2">
         Forgot your password?
-        <router-link class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
-                        cursor-pointer hover:text-jb-textlink-hovered" to="/company/password-forgot">
+        <router-link 
+          class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
+                        cursor-pointer hover:text-jb-textlink-hovered" 
+          to="/company/password-forgot"
+        >
           Reset Your Password
         </router-link>
       </p>
       <p class="text-lg text-jb-subheadings text-center my-2 mb-6">
         Don't have an account?
-        <router-link class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
-                        cursor-pointer hover:text-jb-textlink-hovered" to="/signup/company">
+        <router-link 
+          class="text-jb-textlink font-bold transition-colors duration-200 ease-linear 
+                  cursor-pointer hover:text-jb-textlink-hovered" 
+          to="/signup/company"
+        >
           Create one!
         </router-link>
       </p>
 
-      <button type="submit"
+      <button 
+        type="submit"
         class="bg-jb-textlink rounded-md w-40 h-11 p-2 text-white font-bold text-base 
                border-0 shadow-btn duration-200 ease-linear cursor-pointer hover:bg-jb-btn-hovered hover:shadow-btn-hovered"
         @click="performCompanyLogin()">
