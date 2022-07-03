61f69230c4c4b5938858fe06c1873229b7ccd961
sunny_thinkpad <sunnyw987@gmail.com>
2022-07-03 20:15:39 +1000

    feat(frontend): update page layout of singlejobpage

--- frontend/src/pages/SingleJobPage.vue
+++ frontend/src/pages/SingleJobPage.vue
@@ -7,6 +7,7 @@
     :isOpen="this.isAlertOpen"
     :handleClose="() => { this.isAlertOpen = false }"
   />
+  <!-- TODO replace current 'filled' icons with 'thin' icons -->
   <div style="display: flex; flex-direction: row; justify-content: center; height: 100vh; padding: 0 2rem;">
     <div style="padding: 1rem; background-color: white; border-radius: 10px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25); margin-right: 50px; width: 25%; overflow: scroll">
       <h2 style="font-weight: bold; font-size: 18px; line-height: 27px;">
@@ -16,7 +17,7 @@
       </h2>
     </div>
     <div style="display: flex; flex-direction: column; align-items: center; width: 75%; height: 100%;">
-      <div style="display: flex; flex-direction: row; padding: 1rem; background-color: white; border-radius: 15px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25); margin-bottom: 1.5rem; width: 100%;">
+      <div style="display: flex; flex-direction: row; padding: 1rem; background-color: white; border-radius: 15px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25); margin-bottom: 1rem; width: 100%;">
         <div style="display: flex; flex-direction: column; margin-right: 36px; align-self: center">
           <font-awesome-icon :icon="['fab', 'linkedin']" style="height: 155px; color: #0077B5" />
           <button
@@ -58,66 +59,35 @@
             <font-awesome-icon icon="graduation-cap" class="mr-5 w-7" />
             <b>Required WAM:</b> {{ WamObject[wamRequirements] }}
           </span>
-          <!-- <span style="margin-bottom: 0.25rem;">
+          <span style="margin-bottom: 0.25rem;">
             <font-awesome-icon icon="address-card" class="mr-5 w-7" />
-            <b>Must have one of the following working rights in Australia:</b>
-            <ul class="list-disc list-inside ml-12">
+            <b>{{ ["all"].every((val, idx) => val === this.workingRights[idx]) ? "No required working rights specified for this job listing." : "Must have one of the following working rights in Australia:" }}</b>
+            <ul v-if="!['all'].every((val, idx) => val === this.workingRights[idx])" class="list-disc list-inside ml-12">
               <li v-for="workingRight in workingRights" :key="workingRight">{{ WrObject[workingRight] }}</li>
             </ul>
-          </span> -->
-          <!-- TODO; fix array comparison -->
+          </span>
           <span style="margin-bottom: 0.25rem;">
-              <font-awesome-icon icon="address-card" class="mr-5 w-7" />
-              <b>{{ this.workingRights === ["all"] ? "Student are not required to have working right in Australia to apply." : "Must have one of the following working rights in Australia:" }}</b>
-              <!-- <ul class="list-disc list-inside ml-12">
-                <li v-for="workingRight in workingRights" :key="workingRight">{{ WrObject[workingRight] }}</li>
-              </ul> -->
+            <font-awesome-icon icon="user" class="mr-5 w-7" />
+            <b>{{ ["all"].every((val, idx) => val === this.studentDemographic[idx]) ? "This job listing is open to students at any stage of their degree." : "This job listing is open to only the following students:" }}</b>
+            <ul v-if="!['all'].every((val, idx) => val === this.studentDemographic[idx])" class="list-disc list-inside ml-12">
+              <li v-for="studentType in studentDemographic" :key="studentType">{{ StuDemoObject[studentType] }}</li>
+            </ul>
           </span>
         </div>
       </div>
-      <!-- important info -->
-      <!-- <div class="bg-orange-100 border-t-4 border-orange-500 rounded-b px-4 py-3 shadow-md w-full">
-        <div class="flex">
-          <div class="py-1">
-            <font-awesome-icon icon="circle-info" size="lg"/>
-          </div>
-          <div class="mx-[2%]">
-            <p class="font-bold text-left text-xl">Important Information</p>
-            <p class="text-left" style="font-weight: bold">This position is only open to those that are:</p>
-            <p class="text-left">Important Information</p>
-          </div>
-        </div>
-      </div> -->
-      <div class="text-sm font-medium text-left text-black-500 border-b border-gray-200 dark:text-black-400 dark:border-gray-700" style="width: 100%; margin-bottom: 1rem;">
+      <div style="width: 100%">
         <ul class="flex -mb-px justify-start" style="list-style-position: inside; list-style-type: none">
           <li class="mr-2">
-            <button class="inline-block p-4">Description</button>
+            <button class="inline-block p-4" v-bind:class="[ isJobDescriptionShown ? 'text-blue-600 font-black' : 'text-gray-500 hover:text-gray-700']" @click="() => { isJobDescriptionShown = true }">Description</button>
           </li>
           <li class="mr-2">
-            <button class="inline-block p-4">Additional Information</button>
+            <button class="inline-block p-4" v-bind:class="[ !isJobDescriptionShown ? 'text-blue-600 font-black' : 'text-gray-500 hover:text-gray-700']" @click="() => { isJobDescriptionShown = false }">Additional Information</button>
           </li>
         </ul>
       </div>
-      <!-- <div style="display: flex; flex-direction: row; width: 75%; justify-content: space-between; font-size: 18px; line-height: 27px; margin-bottom: 1rem; align-items: center">
-        <button>Description</button>
-        <p style="color: #C4C4C4; font-size: 5rem">.</p>
-        <button>Additional Information</button>
-      </div> -->
-      <div style="padding: 1rem; background-color: white; border-radius: 15px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25); width: 100%; overflow: scroll">
-        <p v-html="this.description"></p>
-        <p v-html="this.additionalInfo"></p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec justo nibh. Nullam molestie vel sapien ut ultrices. Maecenas efficitur et metus vitae feugiat. Donec in faucibus eros. Donec finibus turpis vitae velit interdum laoreet. Donec pretium diam et erat malesuada, ac rhoncus tortor pulvinar. Duis vel rhoncus massa. Vivamus vitae tempus nisi. Sed blandit nisl nec congue venenatis. Fusce bibendum tellus posuere est consectetur, at interdum ante dapibus. Proin ullamcorper, odio sit amet mattis aliquam, purus quam tristique risus, nec lobortis nibh orci vitae lectus. Donec rhoncus a nibh vehicula tincidunt. In vulputate luctus odio, eget laoreet odio lobortis id.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras euismod interdum nulla, vitae maximus mauris molestie eu. Nulla posuere libero et mi ornare tincidunt. Suspendisse hendrerit neque eu lectus condimentum, pulvinar sodales risus ultrices. Curabitur pellentesque sagittis ipsum vel maximus. Vestibulum pellentesque aliquam mauris id dapibus. Duis ante sem, imperdiet ut odio ac, dictum vehicula nisl. Aliquam mollis augue sit amet ante cursus bibendum. Duis semper fermentum tristique. Donec pretium ante at sapien rutrum, et sodales magna elementum. Sed suscipit efficitur ullamcorper. Aliquam eleifend arcu ac ipsum varius congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam placerat dui vitae rutrum gravida. Phasellus convallis rutrum libero, eu blandit erat suscipit et.
-          Nullam sollicitudin fermentum maximus. Sed varius tortor vel dapibus dignissim. In tempus gravida mauris. Pellentesque dapibus malesuada dapibus. Ut accumsan, nisl sed tempor laoreet, ipsum urna semper ex, sed feugiat justo sapien sit amet tellus. Etiam accumsan purus at quam imperdiet congue. Mauris eget justo dignissim, vehicula purus quis, aliquam sem. Nulla tristique nulla eget placerat hendrerit. Maecenas vehicula aliquet lacus nec placerat. Nam quis sem finibus, elementum enim in, lacinia enim. Donec metus neque, porttitor non mattis sed, cursus ultricies urna. Maecenas ac libero erat.
-          Donec euismod leo tellus, eu congue ex dignissim a. Ut convallis iaculis tellus et tempor. Duis sit amet ante ex. Nulla mattis magna arcu, eu blandit felis cursus nec. Aenean porta scelerisque mauris, ut euismod justo tristique quis. Nullam vitae sollicitudin orci, nec posuere purus. Cras volutpat, massa vel ultricies congue, odio ante dapibus orci, cursus fermentum nibh metus et risus.
-          Pellentesque laoreet tellus urna. Sed dictum ornare erat, eget fermentum augue semper vitae. Nulla porta dapibus rhoncus. In aliquam fringilla enim vitae viverra. Mauris eget dolor eu libero vulputate venenatis at vel dui. Vestibulum ac justo nec libero hendrerit laoreet. Nulla facilisi. Praesent nunc lacus, ultrices non nulla et, pharetra fermentum mi. Nullam vel leo a leo tincidunt luctus. Nulla enim nunc, rhoncus a euismod pharetra, vulputate nec diam. Nulla vel dolor sed lacus facilisis lacinia. Nullam nec purus at nisl dictum blandit. Ut euismod mauris sem, nec rhoncus leo sagittis bibendum. 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec justo nibh. Nullam molestie vel sapien ut ultrices. Maecenas efficitur et metus vitae feugiat. Donec in faucibus eros. Donec finibus turpis vitae velit interdum laoreet. Donec pretium diam et erat malesuada, ac rhoncus tortor pulvinar. Duis vel rhoncus massa. Vivamus vitae tempus nisi. Sed blandit nisl nec congue venenatis. Fusce bibendum tellus posuere est consectetur, at interdum ante dapibus. Proin ullamcorper, odio sit amet mattis aliquam, purus quam tristique risus, nec lobortis nibh orci vitae lectus. Donec rhoncus a nibh vehicula tincidunt. In vulputate luctus odio, eget laoreet odio lobortis id.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras euismod interdum nulla, vitae maximus mauris molestie eu. Nulla posuere libero et mi ornare tincidunt. Suspendisse hendrerit neque eu lectus condimentum, pulvinar sodales risus ultrices. Curabitur pellentesque sagittis ipsum vel maximus. Vestibulum pellentesque aliquam mauris id dapibus. Duis ante sem, imperdiet ut odio ac, dictum vehicula nisl. Aliquam mollis augue sit amet ante cursus bibendum. Duis semper fermentum tristique. Donec pretium ante at sapien rutrum, et sodales magna elementum. Sed suscipit efficitur ullamcorper. Aliquam eleifend arcu ac ipsum varius congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam placerat dui vitae rutrum gravida. Phasellus convallis rutrum libero, eu blandit erat suscipit et.
-          Nullam sollicitudin fermentum maximus. Sed varius tortor vel dapibus dignissim. In tempus gravida mauris. Pellentesque dapibus malesuada dapibus. Ut accumsan, nisl sed tempor laoreet, ipsum urna semper ex, sed feugiat justo sapien sit amet tellus. Etiam accumsan purus at quam imperdiet congue. Mauris eget justo dignissim, vehicula purus quis, aliquam sem. Nulla tristique nulla eget placerat hendrerit. Maecenas vehicula aliquet lacus nec placerat. Nam quis sem finibus, elementum enim in, lacinia enim. Donec metus neque, porttitor non mattis sed, cursus ultricies urna. Maecenas ac libero erat.
-          Donec euismod leo tellus, eu congue ex dignissim a. Ut convallis iaculis tellus et tempor. Duis sit amet ante ex. Nulla mattis magna arcu, eu blandit felis cursus nec. Aenean porta scelerisque mauris, ut euismod justo tristique quis. Nullam vitae sollicitudin orci, nec posuere purus. Cras volutpat, massa vel ultricies congue, odio ante dapibus orci, cursus fermentum nibh metus et risus.
-          Pellentesque laoreet tellus urna. Sed dictum ornare erat, eget fermentum augue semper vitae. Nulla porta dapibus rhoncus. In aliquam fringilla enim vitae viverra. Mauris eget dolor eu libero vulputate venenatis at vel dui. Vestibulum ac justo nec libero hendrerit laoreet. Nulla facilisi. Praesent nunc lacus, ultrices non nulla et, pharetra fermentum mi. Nullam vel leo a leo tincidunt luctus. Nulla enim nunc, rhoncus a euismod pharetra, vulputate nec diam. Nulla vel dolor sed lacus facilisis lacinia. Nullam nec purus at nisl dictum blandit. Ut euismod mauris sem, nec rhoncus leo sagittis bibendum. 
-        </p>
+      <div class="text-left" style="height: 100%; padding: 1rem; background-color: white; border-radius: 15px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25); width: 100%; overflow: scroll">
+        <p v-if="isJobDescriptionShown" v-html="this.description"></p>
+        <p v-else v-html="this.additionalInfo"></p>
       </div>
     </div>
   </div>
@@ -173,7 +143,7 @@ export default Vue.extend({
       location: "Sydney, New South Wales, Australia",
       applicationLink: "www.linkedin.com",
       jobMode: "onsite",
-      studentDemographic: ["penultimate", 'final_year'],
+      studentDemographic: ["all"],
       jobType: "intern",
       workingRights: ["all"],
       additionalInfo: "",
@@ -183,12 +153,12 @@ export default Vue.extend({
       alertMsg: "",
       isAlertOpen: false,
       jobInfoReady: false,
-      // test test
       jobModeObject: JobMode,
-      // StuDemoObject: StudentDemographic,
+      StuDemoObject: StudentDemographic,
       JobTypeObject: JobType,
       WamObject: WamRequirements,
       WrObject: WorkingRights,
+      isJobDescriptionShown: true,
     };
   },
   methods: {
@@ -281,8 +251,6 @@ export default Vue.extend({
   },
   async mounted() {
     // this.fetchJob();
-    console.log(this.workingRights === ["all"])
-    // console.log(this.workingRights.every((val, index) => val === ))
   },
 });
 </script>
