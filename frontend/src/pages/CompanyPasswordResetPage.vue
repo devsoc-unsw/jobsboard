b0af03334a38f41379105003fcae641ecd02162e
joanna209 <joanna.he3@gmail.com>
2022-07-12 21:00:27 +1000

    fix: moved password check logic into  function and resolve input bug

--- frontend/src/pages/CompanyPasswordResetPage.vue
+++ frontend/src/pages/CompanyPasswordResetPage.vue
@@ -17,10 +17,11 @@
       <div class="w-4/5 relative group mt-4 mb-6 sm:w-1/2 md:w-2/5 xl:w-1/4">
         <input 
           name="newPassword" 
+          id="newPassword"
           v-model="newPassword" 
           type="password"
           class="font-bold border-l-4 border-jb-textlink rounded-md p-4 shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full peer"
-          @keyup.enter="checkPasswordMatch()" 
+          @keyup.enter="performCompanyPasswordReset()" 
           required 
         />
         <label 
@@ -38,10 +39,11 @@
       <div class="w-4/5 relative group mt-4 mb-6 sm:w-1/2 md:w-2/5 xl:w-1/4">
         <input 
           name="confirmPassword" 
+          id="confirmPassword"
           v-model="confirmPassword" 
           type="password"
           class="font-bold border-l-4 border-jb-textlink rounded-md p-4 shadow-btn w-full text-lg focus:outline-jb-textlink sm:w-full peer"
-          @keyup.enter="checkPasswordMatch()" 
+          @keyup.enter="performCompanyPasswordReset()" 
           required 
         />
         <label 
@@ -55,7 +57,7 @@
         </label>
       </div>
 
-      <Button class="mt-3" @callback="checkPasswordMatch()">
+      <Button class="mt-3" @callback="performCompanyPasswordReset()">
         <p class="p-4 text-white">Reset Password</p>
       </Button>
     </main>
@@ -91,50 +93,46 @@ export default Vue.extend({
     };
   },
   methods: {
-    async checkPasswordMatch() {
-      if (this.newPassword !== this.confirmPassword) {
+    async performCompanyPasswordReset() {
+      if (this.password !== this.confirmPassword) {
         this.alertType = "error";
         this.alertMsg = "Passwords do not match. Please try again.";
         this.isAlertOpen = true;
       } else {
-        this.performCompanyPasswordReset();
-      }
-    },
+        const response = await fetch(`${config.apiRoot}/company/password-reset`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": this.$route.params.token
+          },
+          // mode: "no-cors",
+          body: JSON.stringify({
+            "newPassword": this.newPassword,
+          }),
+        });
 
-    async performCompanyPasswordReset() {
-      const response = await fetch(`${config.apiRoot}/company/password-reset`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": this.$route.params.token
-        },
-        // mode: "no-cors",
-        body: JSON.stringify({
-          "newPassword": this.newPassword,
-        }),
-      });
-
-      if (response.ok) {
-        window.scrollTo(0, 10);
-        this.alertType = "success";
-        this.isAlertOpen = true;
-        this.alertMsg = "Your password has been successfully been reset. Redirecting you to the login page...";
-        setTimeout(() => {
-          this.$router.push("/login");
-        }, 5000);
-      } else {
-        window.scrollTo(0, 10);
-        this.isAlertOpen = true;
-        this.alertType = "error";
-        if (response.status === 400) {
-          this.alertMsg = "Please try again. Password reset failed.";
-        } else if (response.status === 401) {
-          this.alertMsg = "Token may be invalid or expired. Redirecting to login page.";
+        if (response.ok) {
+          window.scrollTo(0, 10);
+          this.alertType = "success";
+          this.isAlertOpen = true;
+          this.alertMsg = "Your password has been successfully been reset. Redirecting you to the login page...";
           setTimeout(() => {
-            this.$router.push("/login/company");
-          }, 3000);
+            this.$router.push("/login");
+          }, 5000);
         } else {
-          this.alertMsg = "There was an error when trying to reset your password. Please try again.";
+          window.scrollTo(0, 10);
+          this.isAlertOpen = true;
+          this.alertType = "error";
+          if (response.status === 400) {
+            this.alertMsg = "Please try again. Password reset failed.";
+          } else if (response.status === 401) {
+            this.alertMsg = "Token may be invalid or expired. Redirecting to login page.";
+            setTimeout(() => {
+              this.$router.push("/login/company");
+            }, 3000);
+          } else {
+            this.alertMsg = "There was an error when trying to reset your password. Please try again.";
+          }
         }
       }
     },
