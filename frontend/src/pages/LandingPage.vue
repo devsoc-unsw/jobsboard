3a6ad5d7e1236ab944fc4b5e756adf9ed81a63d4
Darian Lee <darianlmj@gmail.com>
2022-08-25 23:34:04 +1000

    feat(frontend): added new jobsboard icon to landing page and adjusted color of blob

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,44 +1,41 @@
 <template>
   <main>
-    <div class="flex flex-col min-h-full">
-      <div class="relative">
-        <Header class="header" />
-      </div>
-      <div class="relative overflow-x-clip">
-        <img 
-          id="blob"
-          :src="Blob" 
-        />
-      </div>
-      <div class="grid grid-rows-2 grid-cols-3 justify-center items-center content-center 
-                  w-[40%] lg:w-[50%] md:w-[60%] h-full ml-[45px] my-12 mx-auto pt-36 mb-32 hero">
-        <div class="self-start col-span-2">
-          <p class="text-white text-left text-lg">CSESoc presents</p>
-          <h1 class="font-bold text-6xl text-[#1a324e] text-left leading-[72px] drop-shadow-lg m-0">Jobs Board</h1>
-          <p class="text-white text-left text-lg">
-            Connecting UNSW students with top employers since 2018.
-          </p>
-        </div>
-        <div class="w-2/4 justify-center items-center content-center justify-items-center col-span-1">
-          <img class="w-full ml-11 sm:w-[100px]" :src="Logo" />
-        </div>
-        <div class="flex flex-row mt-8 col-span-2 self-start justify-self-start">
-          <button 
-            class="bg-[#264c79] rounded-xl shadow-md text-white text-lg font-bold py-[3px] px-8 mt-0 mr-6
-                    hover:duration-500 hover:translate-y-[-2px] hover:shadow-lg sm:mt-2"
-          >
-          Explore
-          </button>
-          <button 
-            class="bg-[#264c79] rounded-xl shadow-md text-white text-lg font-bold py-[3px] px-8 mt-0 mr-6
-                    hover:duration-500 hover:translate-y-[-2px] hover:shadow-lg sm:mt-2"
-          >
-          Advertise
-          </button>
-        </div>
+    <!-- <img id="bigBlob" :src="BigBlob" class="block relative top-0 z-[-1] w-full sm:hidden" />
+    <img id="smallBlob" :src="SmallBlob" class="hidden sm:block sm:absolute sm:z-[-1] sm:w-full" /> -->
+    <div class="flex flex-col h-[70vh] relative sm:mb-[35vh] md:mb-[20vh] blob">
+      <Header class="header" />
 
+      <!-- Blob -->
+
+      <!-- Hero -->
+      <div class="grid grid-rows-1 grid-cols-3 justify-center items-center content-center
+                  w-[40%] lg:w-[50%] md:w-[60%] h-full ml-[45px] my-12 mx-auto pt-36 mb-32 hero">
+        <div class="self-start col-span-2 text-left">
+          <p class="text-lg text-white">CSESoc presents</p>
+          <h1 class="text-[#143A6C] font-bold text-6xl leading-[72px] m-0">Jobs Board</h1>
+          <p class="text-lg text-white">Connecting UNSW students with top employers since 2018.</p>
+          <div class="flex flex-row mt-8 col-span-2 self-start justify-self-start">
+            <button 
+              class="bg-[#264c79] rounded-xl shadow-md text-white text-lg font-bold py-[3px] px-8 mt-0 mr-6
+                      hover:duration-500 hover:translate-y-[-2px] hover:shadow-lg sm:mt-2"
+            >
+              Explore
+            </button>
+            <button 
+              class="bg-[#264c79] rounded-xl shadow-md text-white text-lg font-bold py-[3px] px-8 mt-0 mr-6
+                      hover:duration-500 hover:translate-y-[-2px] hover:shadow-lg sm:mt-2"
+            >
+              Advertise
+            </button>
+          </div>
+        </div>
+        <!-- <div class=" w-2/4 col-span-1 top-[-90px]"> -->
+          <img class="relative w-[80%] top-[-130px] ml-4 sm:ml-8 sm:w-[150%] sm:top-[-50px]" :src="Logo" />
+        <!-- </div> -->
       </div>
-      <div class="w-3/5 mb-12 mx-auto mt-40 xl:mt-0">
+    </div>
+      <!-- Sponsors -->
+      <div class="w-3/5 mb-12 mx-auto">
         <h3 class="font-bold text-3xl mb-0 text-jb-headings">Our Sponsors</h3>
         <p class="text-lg text-jb-subheadings my-4 mx-16 sm:mx-0">
           We aim to give you a pleasant student working experience by partnering up with only the best.
@@ -122,8 +119,9 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import StudentViewTemplate from "@/components/StudentViewTemplate.vue";
-import Blob from "@/assets/misc/Blob.svg";
-import CsesocLogoSmall from "@/assets/logos/CsesocLogoSmall.svg";
+import BigBlob from "@/assets/misc/BigBlob.svg";
+import SmallBlob from "@/assets/misc/SmallBlob.svg";
+import JobsboardLogo from "@/assets/logos/JobsboardLogo.png";
 import Header from "@/components/Header.vue";
 import NewFooter from "@/components/NewFooter.vue";
 import FeaturedJobCard from "@/components/FeaturedJobCard.vue";
@@ -143,8 +141,9 @@ export default Vue.extend({
   data() {
     return {
       GoogleLogo: GoogleLogo,
-      Blob: Blob,
-      Logo: CsesocLogoSmall,
+      BigBlob: BigBlob,
+      SmallBlob: SmallBlob,
+      Logo: JobsboardLogo,
     };
   },
   mounted() {
@@ -199,105 +198,83 @@ main {
   }
 }
 
-#blob {
+#bigBlob {
   position: absolute;
-  width: 370vw;
-  height: 75vh;
-  top: 0;
-  left: -350px;
   z-index: -1;
 
-  @media (min-width: 500px) {
-    width: 200vw;
-    height: 80vh;
-    left: -210px;
+  @media screen and (min-width: 640px) and (max-width: 700px) {
+    width: 250%;
+    left: -300px;
   }
-  @media (min-width: 640px) {
-    width: 200vw;
-    height: 60vh;
-    left: -358px;
+
+  @media screen and (min-width: 700px) and (max-width: 768px) {
+    width: 200%;
+    left: -300px;
   }
-  @media (min-width: 768.1px) {
-    height: 55vh;
-    left: -487px;
-  }
-  @media (min-width: 900px) {
-    width: 150vw;
-    height: 55vh;
+
+  @media screen and (min-width: 768px) and (max-width: 842px) {
+    width: 210%;
     left: -200px;
   }
-  @media (min-width: 1075px) {
-    width: 126vw;
-    height: 52vh;
-    left: -150px;
+
+  @media screen and (min-width: 842px) and (max-width: 880px) {
+    width: 180%;
+    left: -360px;
   }
-  
-  @media (max-height: 1368px) {
-    height: 42vh;
+
+  @media screen and (min-width: 880px) and (max-width: 960px) {
+    width: 150%;
+    left: -50px;
   }
-  
-  @media (max-height: 978px) {
-    height: 66vh;
-    left: -200px;
-    
-    @media (max-width: 1075px) {
-      height: 65vh;
-      left: -255px;
-    }
-    @media (max-width: 900px) {
-      height: 61vh;
-      left: -460px;
-    }
-    @media (max-width: 761.8px) {
-      height: 65vh;
-      left: -415px;
-    }
-    @media (max-width: 640px) {
-      height: 96vh;
-      left: -254px;
-    }
-    @media (max-width: 498px) {
-      height: 85vh;
-      left: -965px;
-    }
-    @media (max-width: 395px) {
-      height: 109vh;
-      left: -734px;
-    }
+
+  @media screen and (min-width: 960px) and (max-width: 1024px) {
+    width: 140%;
+    left: -100px;
   }
-  
-    @media (max-height: 800px) {
-    height: 66vh;
-    left: -200px;
-    
-    @media (max-width: 1075px) {
-      height: 73vh;
-      left: -255px;
-    }
-    @media (max-width: 900px) {
-      height: 70vh;
-      left: -460px;
-    }
-    @media (max-width: 761.8px) {
-      height: 83vh;
-      left: -415px;
-    }
-    @media (max-width: 640px) {
-      height: 110vh;
-      left: -254px;
-    }
-    @media (max-width: 498px) {
-      height: 110vh;
-      left: -965px;
-    }
-    @media (max-width: 395px) {
-      height: 109vh;
-      left: -734px;
-    }
+
+  @media screen and (min-width: 1024px) and (max-width: 1050px) {
+    width: 150%;
+    left: -100px;
   }
-  
-  @media (max-height: 667px) {
-    height: 130vh;
+
+  @media screen and (min-width: 1050px) and (max-width: 1150px) {
+    width: 135%;
+    left: -180px;
+  }
+
+  @media screen and (min-width: 1150px) and (max-width: 1280px) {
+    width: 115%;
   }
 }
+
+#smallBlob {
+  @media screen 
+  and (min-width: 400px) 
+  and (max-width: 480px) {
+    top: -100px;
+  }
+
+  @media screen and (min-width: 480px) and (max-width: 500px) {
+    top: -180px;
+  }
+
+  @media screen and (min-width: 500px) and (max-width: 560px) {
+    top: -280px;
+  }
+
+  @media screen and (min-width: 560px) and (max-width: 600px) {
+    top: -380px;
+  }
+
+  @media screen and (min-width: 600px) and (max-width: 640px) {
+    top: -480px;
+  }
+}
+
+.blob {
+  background-image: url('@/assets/misc/BigBlob.svg');
+  background-repeat: no-repeat;
+  background-size: 100vw;
+}
+
 </style>
