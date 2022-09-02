a3754d724554ee3965e7e231472e66d65c8c24ca
sunny_thinkpad <sunnyw987@gmail.com>
2022-09-02 18:39:26 +1000

    feat(frontend): update eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     es2021: true,
+  },  
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: "module",
   },
-  parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
   ],
@@ -11,6 +16,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:node/recommended'
+    "plugin:vue/recommended"
   ],
 };
