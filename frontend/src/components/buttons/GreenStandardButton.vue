eab75e98831b0f55e95409d2bf7288b295a2d0ed
adriantan09 <adrian.tan09@gmail.com>
2022-09-22 21:25:39 +1000

    feat(frontend): buttons support custom padding/margin

--- frontend/src/components/buttons/GreenStandardButton.vue
+++ frontend/src/components/buttons/GreenStandardButton.vue
@@ -8,25 +8,25 @@
 </script>
 
 <style scoped lang="scss">
-button {
+:slotted(button) {
   border-width: 0px;
   border-left-style: solid;
   border-left-width: 5px;
   border-left-color: $green;
   background: $white;
   /* border-radius: 0.2rem; */
-  color: $black;
+  color: $green;
   text-align: center;
   width: fit-content();
-  padding: 1rem;
-  margin: 1rem;
+  /* padding: 1rem;
+  margin: 1rem; */
   /* offset-x | offset-y | blur-radius | spread-radius | color */
   box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.1);
-  min-width: 50%;
+  /* min-width: 50%; */
   max-width: 100%;
 }
 
-button:hover {
+:slotted(button:hover) {
   background: $green;
   color: $white;
 }
