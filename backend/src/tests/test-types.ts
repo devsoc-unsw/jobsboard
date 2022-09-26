201a8a527ef9462df94a3838e98a6926d88a3ea2
Matthew Liu <mattliurw@gmail.com>
2022-09-26 16:28:14 +1000

    feat (backend): added new types for creating testing data

---
+++ backend/src/tests/test-types.ts
@@ -0,0 +1,31 @@
+
+export interface AdminAccountInterface {
+    username: string,
+    password: string
+};
+
+export interface CompanyAccountInterface {
+    username: string,
+    password: string,
+    verified: boolean,
+    name: string,
+    location: string
+};  
+
+export interface JobInterface {
+    title: string,
+    company_username: string,
+    role: string,
+    description: string,
+    application_link: string,
+    approved: boolean,
+    hidden: boolean,
+    mode: string,
+    student_demographic: string[],
+    type: string,
+    working_rights: string[],
+    wam_requirements: string,
+    additional_info: string,
+    paid: boolean,
+    expiry: string
+};
