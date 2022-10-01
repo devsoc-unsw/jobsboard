b59f7ff5718a32179c7dbd80e2b5902849d1c5a3
Darian Lee <darianlmj@gmail.com>
2022-10-01 14:47:08 +1000

    style(frontend): modified styling for logo upload modal

--- frontend/src/pages/CompanyAccountHome.vue
+++ frontend/src/pages/CompanyAccountHome.vue
@@ -8,27 +8,27 @@
         <!-- Modal -->
         <div
           tabindex='-1'
-          class='overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'
+          class='overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none flex justify-center items-center'
         >
           <div class='relative p-4 w-1/2 mx-auto max-w-full'>
             <!-- Modal content -->
-            <div class='relative bg-white rounded-lg bg-gray-700'>
+            <div class='relative rounded-lg bg-white'>
               <!-- Modal header -->
-              <div class='flex justify-between items-center p-5 rounded-t border-b border-gray-600'>
-                <h2 class='text-xl font-medium text-white'>
+              <div class='flex justify-between items-center p-5 rounded-t border-gray-600'>
+                <h2 class='text-xl font-medium text-jb-subheader'>
                   Looks like there isn't a company logo associated with this account.
                 </h2>
               </div>
               <!-- Modal body -->
               <div class='flex justify-center items-center w-full p-6'>
-                <label class='flex flex-col justify-center items-center w-full h-64 rounded-lg border-2 border-dashed cursor-pointer bg-gray-700 border-gray-600 hover:border-gray-500 hover:bg-gray-600'>
+                <label class='flex flex-col justify-center items-center w-full h-64 rounded-lg border-2 border-dashed cursor-pointer bg-white border-gray-600 hover:border-gray-500 hover:bg-gray-100'>
                   <div class='flex flex-col justify-center items-center pt-5 pb-6'>
                     <font-awesome-icon
                       icon='cloud-upload'
-                      class='text-white mb-4'
+                      class='text-jb-subheader mb-4'
                       size='3x'
                     />
-                    <p class='mb-2 text-sm text-white'>
+                    <p class='mb-2 text-sm'>
                       {{ logo ? logo.name: 'Click to upload an image' }}
                     </p>
                   </div>
@@ -41,7 +41,7 @@
                 </label>
               </div>
               <!-- Modal footer -->
-              <div class='flex flex-row justify-end p-6 space-x-2 rounded-b border-t border-gray-600'>
+              <div class='flex flex-row justify-end p-6 space-x-2 rounded-b border-gray-600'>
                 <button
                   class='bg-red-600 rounded-md text-white font-bold text-base border-0 px-6 py-2 shadow-md duration-200 ease-linear cursor-pointer hover:bg-red-700 hover:shadow-md-hovered'
                   @click='() => isModalShown = false'
