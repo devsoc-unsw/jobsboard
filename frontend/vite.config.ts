b6c0d945c4d35deffaf662b3ce86e8ff304eb7f2
Gabriel <gabriel.ting@nine.com.au>
2022-10-27 01:14:15 +1100

    change quotes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import \'./src/style/_variables.scss\';',
+        additionalData: '@import "./src/style/_variables.scss";',
       },
     },
   },
