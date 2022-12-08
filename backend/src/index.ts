ec2978896c0402b50e44e503ac37a1efa3e2ddc3
sunny_thinkpad <sunnyw987@gmail.com>
2022-12-09 10:33:36 +1100

    chore(backend): replace depracated package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,9 +43,8 @@ Logger.Init();
 
 const app = express();
 const port = process.env.SERVER_PORT;
-app.use(bodyParser.json({
-  limit: '5mb'
-}));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb' }));
 app.use(helmet());
 
 let corsOptions;
