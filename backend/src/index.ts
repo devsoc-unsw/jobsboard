154c9193400fbf4c5c4edc9952e3a45c6ed3bd7a
Sunny <66097119+casio987@users.noreply.github.com>
2022-12-11 12:16:42 +1100

    fix(backend): request size limit (#283)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,7 +43,11 @@ Logger.Init();
 
 const app = express();
 const port = process.env.SERVER_PORT;
-app.use(bodyParser.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ 
+  limit: '5mb',
+  extended: true
+}));
 app.use(helmet());
 
 let corsOptions;
