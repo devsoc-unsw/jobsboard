eb34fb1a4eff95b7fc6a4fd57a9b7ad0b5ccf5bd
Joanna He <joanna.he3@gmail.com>
2022-12-15 15:09:36 +1100

    chore(frontend): uncomment config url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
-  // apiRoot: 'https://jobsboard.csesoc.unsw.edu.au/api',
+  apiRoot: 'https://jobsboard.csesoc.unsw.edu.au/api',
   // apiRoot: 'https://jobsboard.staging.csesoc.unsw.edu.au/api',
-  apiRoot: 'http://127.0.0.1:8080',
+  // apiRoot: 'http://127.0.0.1:8080',
   sessionStorageApiTokenKeyName: 'jobs-board-api-token',
 };
