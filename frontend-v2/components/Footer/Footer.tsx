03eec1e6dd00ed265894495461bb09b3e0915922
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-12-25 22:11:24 +1100

    fix: uadjust flex box for footer and used fontawesome icons

--- frontend-v2/components/Footer/Footer.tsx
+++ frontend-v2/components/Footer/Footer.tsx
@@ -1,123 +1,93 @@
 "use client"
 import Image from 'next/image';
 import logo from 'assets/logos/csesocwhite.png';
-import facebookLogo from 'assets/SocialMedia/facebook.svg';
-import instagramLogo from 'assets/SocialMedia/instagram.svg';
-import linkedinLogo from 'assets/SocialMedia/linkedin.svg';
-import youtubeLogo from 'assets/SocialMedia/youtube.svg';
-import discordLogo from 'assets/SocialMedia/discord.svg';
-import githubLogo from 'assets/SocialMedia/github.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import container from 'styles/container.module.css';
-import { useRouter } from 'next/navigation';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faDiscord, faFacebook, faGithub, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
 
 const Footer = () => {
-  const router = useRouter()
+  const socials = [
+    {
+      title: 'Facebook',
+      icon: faFacebook,
+      href: 'https://www.facebook.com/csesoc'
+    },
+    {
+      title: 'Instagram',
+      icon: faInstagram,
+      href: 'https://www.instagram.com/csesoc_unsw/?hl=en',
+    },
+    {
+      title: 'LinkedIn',
+      icon: faLinkedin,
+      href: 'https://au.linkedin.com/company/csesoc',
+    },
+    {
+      title: 'YouTube',
+      icon: faYoutube,
+      href: 'https://www.youtube.com/channel/UC1JHpRrf9j5IKluzXhprUJg',
+    },
+    {
+      title: 'Discord',
+      icon: faDiscord,
+      href: 'https://bit.ly/CSESocDiscord',
+    },
+    {
+      title: 'Github',
+      icon: faGithub,
+      href: 'https://github.com/csesoc',
+    },
+  ]
+
   return (
     <div className='bg-jb-blue md:flex-col text-white py-5'>
-    <div className={`flex justify-between items-center ${container.pageContainer}`}>
-      {/* TODO <style scoped lang="scss">
-      a {
-        display: flex;
-        justify-content: center;
-        color: #ffffff;
-      }
-      </style> */}
-      <div>
-        <Image
-          alt='logo'
-          className='inline pt-4 pr-4 pl-4 w-1/2 lg:w-2/3 sm:w-1/2'
-          src={logo}
-        />
-        <div className='py-2.5 font-bold'>
+      <div className={`flex justify-between items-center ${container.pageContainer}`}>
+        <div className='flex flex-col items-center font-bold gap-3'>
+          <Image
+            alt='logo'
+            src={logo}
+            width={250}
+          />
           <a
-            className='py-2.5'
             href='https://docs.google.com/forms/d/e/1FAIpQLSeldliO0mbbbJWvJk3CoUhiN-ACwmHGnX-e3xtO-l4rGk9bPg/viewform'
-            target='_blank' rel="noreferrer"
+            target='_blank'
+            rel="noreferrer"
           >
             Got a Question?
           </a>
-          <a
+          <Link
             className='cursor-pointer'
-            onClick={() => router.push('/team')}
-          >Meet the Team</a>
+            href="/team"
+          >Meet the Team
+          </Link>
+          <p className='m-0'>
+            &#169; CSESoc UNSW - 2022
+          </p>
+          <div className='flex justify-center py-[15px] gap-5'>
+            {socials.map((s) => <a href={s.href} target='__blank' key={s.title}>
+              <FontAwesomeIcon icon={s.icon} size="lg"/>
+            </a>)}
+          </div>
         </div>
-        <p className='m-0'>
-          &#169; CSESoc UNSW - 2022
-        </p>
-        <div className='flex justify-center py-[15px]'>
-          <a
-            href='https://www.facebook.com/csesoc'
-            target='__blank'
-          >
-            <Image
-            alt='facebook'
-              className='block w-1/2'
-              src={facebookLogo}
-            />
-          </a>
-          <a
-            href='https://www.instagram.com/csesoc_unsw/?hl=en'
-            target='__blank'
-          >
-            <Image
-              alt='instagram'
-              className='block w-1/2'
-              src={instagramLogo}
-            />
-          </a>
-          <a
-            href='https://au.linkedin.com/company/csesoc'
-            target='__blank'
-          >
-            <Image
-                className='block w-1/2'
-                src={linkedinLogo} alt={''}          />
-          </a>
-          <a
-            href='https://www.youtube.com/channel/UC1JHpRrf9j5IKluzXhprUJg'
-            target='__blank'
-          >
-            <Image
-                className='block w-1/2'
-                src={youtubeLogo} alt={''}          />
-          </a>
-          <a
-            href='https://bit.ly/CSESocDiscord'
-            target='__blank'
-          >
-            <Image
-                className='block w-1/2'
-                src={discordLogo} alt={''}          />
-          </a>
-          <a
-            href='https://github.com/csesoc'
-            target='__blank'
-          >
-            <Image
-                className='block w-1/2'
-                src={githubLogo} alt={''}          />
-          </a>
-        </div>
-      </div>
 
-      <div className='flex flex-col justify-center items-center text-justify px-[30px]'>
-        <p className='m-0'>
-          CSESoc is the constituent student society of UNSW&apos;s School of Computer Science
-          and Engineering. We do not represent the School, Faculty, or University.
-          This website seeks to be a centralised platform for students looking
-          for employment opportunities, but its information has not been officially
-          endorsed by  the University, Faculty, School, or the Computer Science and
-          Engineering Society.  You should confirm with the employer that any information
-          received through this website is correct.
-          <br/>
-          <br/>
-          Jobsboard was made with <FontAwesomeIcon icon={faHeart} /> by CSE students for CSE students.
-        </p>
+        <div className='flex flex-col justify-center items-center text-justify px-[30px]'>
+          <p className='m-0'>
+            CSESoc is the constituent student society of UNSW&apos;s School of Computer Science
+            and Engineering. We do not represent the School, Faculty, or University.
+            This website seeks to be a centralised platform for students looking
+            for employment opportunities, but its information has not been officially
+            endorsed by  the University, Faculty, School, or the Computer Science and
+            Engineering Society.  You should confirm with the employer that any information
+            received through this website is correct.
+            <br/>
+            <br/>
+            Jobsboard was made with <FontAwesomeIcon icon={faHeart} /> by CSE students for CSE students.
+          </p>
+        </div>
       </div>
-  </div>
-  </div>
+    </div>
   )
 }
 
