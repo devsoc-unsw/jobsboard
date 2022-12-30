3a700d9c3024806eeafa6a1142fd4b1370fd35fa
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-12-30 16:36:11 +1100

    fix: used session token as fallback

--- frontend/app/AppProvider.tsx
+++ frontend/app/AppProvider.tsx
@@ -6,14 +6,22 @@ type Props = {
   children: React.ReactNode;
 };
 
+const sessionStorageApiTokenKeyName = 'jobs-board-api-token';
+
 const AppProvider = ({ children }: Props) => {
   const [apiToken, setApiToken] = useState<string | null>(null);
   return (
     <AppContext.Provider
       value={{
-        apiToken,
-        setApiToken: (api) => setApiToken(api),
-        resetApiToken: () => setApiToken(null)
+        apiToken: apiToken || sessionStorage.getItem(sessionStorageApiTokenKeyName),
+        setApiToken: (token) => {
+          setApiToken(token);
+          sessionStorage.setItem(sessionStorageApiTokenKeyName, token);
+        },
+        resetApiToken: () => {
+          setApiToken(null);
+          sessionStorage.removeItem(sessionStorageApiTokenKeyName);
+        }
       }}
     >
       {children}
