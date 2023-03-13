7989ac3e665ac1a330357c9e0332016d018bddb1
Matthew Liu <mattliurw@gmail.com>
2023-03-13 22:55:15 +1100

    reverted change to config.js since it's only required for local dev

--- backend/tests/config.js
+++ backend/tests/config.js
@@ -1,9 +1,9 @@
 const config = {
   // only use when developing locally
-  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'http://localhost:8080',
 
   // use otherwise (not sure what this does)
-  // apiUrl: 'http://api:8080',
+  apiUrl: 'http://api:8080',
 };
 
 module.exports = config;
