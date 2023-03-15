b5d8c1fb799e3d74f97cf6b8a863bf84603272be
Matthew Liu <mattliurw@gmail.com>
2023-03-15 20:38:58 +1100

    updated documentation in config.js

--- backend/tests/config.js
+++ backend/tests/config.js
@@ -2,7 +2,7 @@ const config = {
   // only use when developing locally
   // apiUrl: 'http://localhost:8080',
 
-  // use otherwise (not sure what this does)
+  // switch back when submitting pr
   apiUrl: 'http://api:8080',
 };
 
