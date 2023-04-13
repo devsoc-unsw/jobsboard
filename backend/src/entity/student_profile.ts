031af22a986697bf919c8f586ecd4dd331809a16
William Setiawan <z5388080@ad.unsw.edu.au>
2023-04-14 09:59:03 +1000

    Added StudentProfile incomplete boilerplate

---
+++ backend/src/entity/student_profile.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+} from 'typeorm';
+import Company from './company';
+
+@Entity()
+export default class StudentProfile {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public username: string;
+
+  @Column()
+  public hash: string;
+
+  @OneToOne((_) => Company, (company) => company.companyAccount, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  public company: Company;
+
+  @Column({
+    default: false,
+  })
+  public verified: boolean;
+
+  @Column({ type: 'text', default: 'no token set' })
+  public latestValidToken: string;
+
+  @Column({ type: 'text', default: 'no token set' })
+  public latestValidResetToken: string;
+
+  @CreateDateColumn()
+    createdAt: Date;
+
+  @UpdateDateColumn()
+    updatedAt: Date;
+}
