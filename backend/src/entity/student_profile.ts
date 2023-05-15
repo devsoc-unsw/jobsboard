b5b29cf6cfba0388758c3f20388958e27c58267b
William Setiawan <z5388080@ad.unsw.edu.au>
2023-05-16 02:19:29 +1000

    Added startYear and wam fields to StudentProfile

--- backend/src/entity/student_profile.ts
+++ backend/src/entity/student_profile.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   JoinColumn,
+  Column,
 } from 'typeorm';
 import Student from './student';
 
@@ -12,6 +13,12 @@ import Student from './student';
 export default class StudentProfile {
   @PrimaryGeneratedColumn()
   public id: number;
+
+  @Column()
+  public startYear: number;
+
+  @Column({ default: 0 })
+  public wam: number;
 
   @OneToOne((_) => Student, (student) => student.studentProfile, {
     cascade: true,
