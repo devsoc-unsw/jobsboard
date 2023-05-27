7065cbbf81b68e9a321b1e45cf21bc08190f42fc
Matthew Liu <mattliurw@gmail.com>
2023-05-27 14:45:50 +1000

    set apiURL

--- backend/tests_new/config.ts
+++ backend/tests_new/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   // use in local development
-  apiUrl: 'http://localhost:8080/',
+  // apiUrl: 'http://localhost:8080/',
 
   // use in staging and production
-  // apiUrl: 'http://api:8080',
+  apiUrl: 'http://api:8080',
 };
 
 // refer to https://stackoverflow.com/a/31354244/15009279
