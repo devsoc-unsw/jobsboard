027e23e6a8a314f61c1cd0e091a3c3dc05c08a12
Matthew Liu <mattliurw@gmail.com>
2023-05-27 13:00:21 +1000

    linted db helpers for testing + remove use of async-await in loop

--- backend/tests_new/test_lib/helpers.ts
+++ backend/tests_new/test_lib/helpers.ts
@@ -1,14 +1,18 @@
-import { AppDataSource } from "../../src/config";
+import { AppDataSource } from '../../src/config';
 
 export const seedDB = async () => {
   await AppDataSource.initialize();
   await AppDataSource.synchronize(true);
-}
+};
 
 export const clearDB = async () => {
   const entities = AppDataSource.entityMetadatas;
-  for (const entity of entities) {
-    const repo = AppDataSource.getRepository(entity.name);
-    await repo.clear();
-  }
-}
+  const promises: Promise<void>[] = [];
+
+  entities.forEach((entity) => {
+    const repository = AppDataSource.getRepository(entity.name);
+    promises.push(repository.clear());
+  });
+
+  await Promise.all(promises);
+};
