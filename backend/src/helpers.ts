632417ab77f3812506a5c38078f62a64f7cee6c7
Matthew Liu <mattliurw@gmail.com>
2023-05-29 12:27:45 +1000

    rebase 3/19

--- backend/src/helpers.ts
+++ backend/src/helpers.ts
@@ -50,19 +50,17 @@ export default class Helpers {
     throw new Error(`Invalid mailto or HTTP[S] application link: ${value}`);
   }
 
-  public static async doSuccessfullyOrFail
-    <TReturn>(
+  public static async doSuccessfullyOrFail<TReturn>(
     func: () => Promise<TReturn>,
     failMessage: string,
-  ): Promise<TReturn>
-    {
-      const res = await func();
-      if (res === undefined) {
-        Logger.Error(LM, failMessage);
-        throw new Error(failMessage);
-      }
-      return res;
+  ): Promise<TReturn> {
+    const res = await func();
+    if (res === undefined) {
+      Logger.Error(LM, failMessage);
+      throw new Error(failMessage);
     }
+    return res;
+  }
 
   public static async catchAndLogError(
     res: Response,
@@ -73,12 +71,10 @@ export default class Helpers {
     let response: IResponseWithStatus;
     try {
       response = await func();
-    }
-    catch (error: unknown) {
+    } catch (error: unknown) {
       if (error instanceof Error) {
         Logger.Error(LM, `EXCEPTION: ${error.name} - ${error.message}\nSTACK:\n${error.stack}`);
-      }
-      else {
+      } else {
         Logger.Error(LM, 'Unknown error was thrown');
       }
       response = funcOnError();
@@ -86,12 +82,10 @@ export default class Helpers {
     if (!res.headersSent) {
       if (response.msg === undefined) {
         res.sendStatus(response.status);
-      }
-      else {
+      } else {
         res.status(response.status).send(response.msg);
       }
-    }
-    else {
+    } else {
       Logger.Error(LM, 'Not performing any further action as headers are already sent.');
     }
     if (next) next();
--- backend/src/jwt.ts
+++ backend/src/jwt.ts
@@ -17,16 +17,12 @@ export enum AccountType {
 
 export default class JWT {
   public static create(raw: object): string {
-    // create a string from the object
     const stringObject = JSON.stringify(raw);
-    // encrypt
     return Secrets.encrypt(stringObject);
   }
 
   public static get(msg: string): IToken {
-    // decrypt
     const raw = Secrets.decrypt(msg);
-    // convert back to JSON object
     return JSON.parse(raw) as IToken;
   }
 }
