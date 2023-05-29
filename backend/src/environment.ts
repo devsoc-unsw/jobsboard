d94420ce2531489996d838baf12638af9ae95259
Matthew Liu <mattliurw@gmail.com>
2023-05-29 14:38:50 +1000

    test

--- backend/src/environment.ts
+++ backend/src/environment.ts
@@ -1,20 +1,43 @@
 import z from 'zod';
 import dotenv from 'dotenv';
+import { createEnv } from '@t3-oss/env-core';
+
+dotenv.config();
+
+export const env = createEnv({
+  clientPrefix: undefined,
+  server: {
+    NODE_ENV: z.enum(['development', 'production']),
+    SERVER_PORT: z.string().min(1),
+    DB_HOST: z.string().min(1),
+    DB_PORT: z.string().min(1),
+    DB_USER: z.string().min(1),
+    DB_PASSWORD: z.string().min(1),
+    DB_NAME: z.string().min(1),
+    MAIL_USERNAME: z.string().min(1),
+    MAIL_PASSWORD: z.string().min(1),
+    MAIL_SMTP_SERVER: z.string().min(1),
+    MAIL_SMTP_SERVER_PORT: z.string().min(1),
+  },
+  client: undefined,
+  runtimeEnv: process.env,
+  isServer: true,
+});
 
 // ensure relevant environment variables exist
-const readerSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production']),
-  SERVER_PORT: z.string().min(1),
-  DB_HOST: z.string().min(1),
-  DB_PORT: z.string().min(1),
-  DB_USER: z.string().min(1),
-  DB_PASSWORD: z.string().min(1),
-  DB_NAME: z.string().min(1),
-  MAIL_USERNAME: z.string().min(1),
-  MAIL_PASSWORD: z.string().min(1),
-  MAIL_SMTP_SERVER: z.string().min(1),
-  MAIL_SMTP_SERVER_PORT: z.string().min(1),
-});
+// const readerSchema = z.object({
+//   NODE_ENV: z.enum(['development', 'production']),
+//   SERVER_PORT: z.string().min(1),
+//   DB_HOST: z.string().min(1),
+//   DB_PORT: z.string().min(1),
+//   DB_USER: z.string().min(1),
+//   DB_PASSWORD: z.string().min(1),
+//   DB_NAME: z.string().min(1),
+//   MAIL_USERNAME: z.string().min(1),
+//   MAIL_PASSWORD: z.string().min(1),
+//   MAIL_SMTP_SERVER: z.string().min(1),
+//   MAIL_SMTP_SERVER_PORT: z.string().min(1),
+// });
 
 // ensure relavant environment variables are of the correct type
 const envSchema = z.object({
@@ -42,25 +65,22 @@ class Environment {
   }
 
   private static parse(): EV {
-    dotenv.config();
-
-    const preprocessed = readerSchema.safeParse(process.env);
-    if (!preprocessed.success) {
-      throw new Error('Unable to read environment variables. Exiting.');
-    }
-
+    // const preprocessed = readerSchema.safeParse(process.env);
+    // if (!preprocessed.success) {
+    //   throw new Error('Unable to read environment variables. Exiting.');
+    // }
     const parsed: EV = {
-      NODE_ENV: preprocessed.data.NODE_ENV,
-      SERVER_PORT: parseInt(preprocessed.data.SERVER_PORT, 10),
-      DB_HOST: preprocessed.data.DB_HOST,
-      DB_PORT: parseInt(preprocessed.data.DB_PORT, 10),
-      DB_USER: preprocessed.data.DB_USER,
-      DB_PASSWORD: preprocessed.data.DB_PASSWORD,
-      DB_NAME: preprocessed.data.DB_NAME,
-      MAIL_USERNAME: preprocessed.data.MAIL_USERNAME,
-      MAIL_PASSWORD: preprocessed.data.MAIL_PASSWORD,
-      MAIL_SMTP_SERVER: preprocessed.data.MAIL_SMTP_SERVER,
-      MAIL_SMTP_SERVER_PORT: parseInt(preprocessed.data.MAIL_SMTP_SERVER_PORT, 10),
+      NODE_ENV: env.NODE_ENV,
+      SERVER_PORT: parseInt(env.SERVER_PORT, 10),
+      DB_HOST: env.DB_HOST,
+      DB_PORT: parseInt(env.DB_PORT, 10),
+      DB_USER: env.DB_USER,
+      DB_PASSWORD: env.DB_PASSWORD,
+      DB_NAME: env.DB_NAME,
+      MAIL_USERNAME: env.MAIL_USERNAME,
+      MAIL_PASSWORD: env.MAIL_PASSWORD,
+      MAIL_SMTP_SERVER: env.MAIL_SMTP_SERVER,
+      MAIL_SMTP_SERVER_PORT: parseInt(env.MAIL_SMTP_SERVER_PORT, 10),
     };
 
     const validated = envSchema.safeParse(parsed);
