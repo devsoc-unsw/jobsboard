bcb203ab742b399eefdd4e360c78bfd283d8344d
adriantan09 <64292824+adriantan09@users.noreply.github.com>
2023-08-29 18:41:38 +1000

    JOB-148 FE revamp job info page [TAKE 2] (#342)

---
+++ frontend/components/OtherJobs/OtherJobs.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import JobListingMinimal from 'components/JobListingMinimal/JobListingMinimal';
+import { Job, JobWithCompany } from 'types/api';
+
+type JobCardProps = {
+  job: JobWithCompany;
+  companyJobs: Job[];
+  className?: string | undefined;
+};
+
+const JobCard = ({ job, companyJobs, className }: JobCardProps) => {
+  const outsideClass = className || '';
+
+  return (
+    <div
+      className={`${outsideClass} flex flex-col py-4 px-2 h-full max-h-96 bg-white rounded-lg overflow-y-auto shadow-card`}
+    >
+      <h2
+        className={`font-bold text-lg text-center text-jb-headings ${
+          companyJobs.length === 0 ? 'my-auto' : 'mb-4'
+        }`}
+      >
+        {companyJobs.length === 0
+          ? 'There are no other jobs from this company.'
+          : 'Other jobs from this company'}
+      </h2>
+      <div className="flex flex-col md:flex-row justify-between overflow-auto">
+        {companyJobs.map((companyJob) => (
+          <JobListingMinimal
+            key={companyJob.id}
+            id={companyJob.id}
+            role={companyJob.role}
+            company={job.company.name}
+            location={job.company.location}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
