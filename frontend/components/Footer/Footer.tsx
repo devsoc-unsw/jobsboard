4fa9698a3b29e848fc42a49af725148a9158b0bf
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2024-01-29 12:07:34 +1100

    Relinked Socials

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -1,52 +1,35 @@
 'use client';
 
 import React from 'react';
-import {
-  faDiscord,
-  faFacebook,
-  faGithub,
-  faInstagram,
-  faLinkedin,
-  faYoutube
-} from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
 import container from 'styles/container.module.css';
-import logo from 'assets/logos/devsoc.png';
+import logo from 'assets/logos/devsocwhite.svg';
 
 const Footer = () => {
   const socials = [
     {
       title: 'Facebook',
       icon: faFacebook,
-      href: 'https://www.facebook.com/csesoc'
+      href: 'https://www.facebook.com/devsocUNSW'
     },
     {
       title: 'Instagram',
       icon: faInstagram,
-      href: 'https://www.instagram.com/csesoc_unsw/?hl=en'
+      href: 'https://www.instagram.com/devsoc_unsw/'
     },
     {
       title: 'LinkedIn',
       icon: faLinkedin,
-      href: 'https://au.linkedin.com/company/csesoc'
-    },
-    {
-      title: 'YouTube',
-      icon: faYoutube,
-      href: 'https://www.youtube.com/channel/UC1JHpRrf9j5IKluzXhprUJg'
-    },
-    {
-      title: 'Discord',
-      icon: faDiscord,
-      href: 'https://bit.ly/CSESocDiscord'
+      href: 'https://www.linkedin.com/company/devsoc-unsw/'
     },
     {
       title: 'Github',
       icon: faGithub,
-      href: 'https://github.com/csesoc'
+      href: 'https://github.com/devsoc-unsw'
     }
   ];
 
@@ -65,7 +48,7 @@ const Footer = () => {
           <Link className="cursor-pointer" href="/team">
             Meet the Team
           </Link>
-          <p className="m-0">&#169; CSESoc UNSW - 2022</p>
+          <p className="m-0">&#169; DevSoc UNSW - 2022</p>
           <div className="flex justify-center py-[15px] gap-5">
             {socials.map((s) => (
               <a href={s.href} target="__blank" key={s.title}>
@@ -77,12 +60,12 @@ const Footer = () => {
 
         <div className="flex flex-col justify-center items-center text-justify px-[30px]">
           <p className="m-0">
-            CSESoc is the constituent student society of UNSW&apos;s School of Computer Science and
-            Engineering. We do not represent the School, Faculty, or University. This website seeks
-            to be a centralised platform for students looking for employment opportunities, but its
-            information has not been officially endorsed by the University, Faculty, School, or the
-            Computer Science and Engineering Society. You should confirm with the employer that any
-            information received through this website is correct.
+            DevSoc is the UNSW Software Development Society. We do not represent the School,
+            Faculty, or University. This website seeks to be a centralised platform for students
+            looking for employment opportunities, but its information has not been officially
+            endorsed by the University, Faculty, School, or the Computer Science and Engineering
+            Society. You should confirm with the employer that any information received through this
+            website is correct.
             <br />
             <br />
             Jobsboard was made with <FontAwesomeIcon icon={faHeart} /> by CSE students for CSE
