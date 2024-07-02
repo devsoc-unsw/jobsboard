1becb074b42c7d42e1c0ae2c5341528037b57c75
alec <z5260142@ad.unsw.edu.au>
2024-07-02 23:15:51 +1000

    made changes to profilecard to hover linkedin and gh icons (no shadow).

--- frontend/components/ProfileCard/ProfileCard.tsx
+++ frontend/components/ProfileCard/ProfileCard.tsx
@@ -30,11 +30,23 @@ const ProfileCard = ({
             <div className="w-24 h-24">
               <img alt={name} src={photo} className="w-28 h-28" />
               <div className="flex justify-items-center gap-3 mt-6">
-                <a href={linkedin} target="__blank">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-blue-700 h-8" />
+                <a 
+                href={linkedin} 
+                target="__blank"
+                className="transition-transform duration-300 hover:translate-y-[-4px]">
+                  <FontAwesomeIcon 
+                  icon={faLinkedin} 
+                  size="2x" 
+                  className="text-blue-700 h-8" />
                 </a>
-                <a href={gh} target="__blank">
-                  <FontAwesomeIcon icon={faGithub} size="2x" className="h-8" />
+                <a 
+                href={gh} 
+                target="__blank" 
+                className="transition-transform duration-300 hover:translate-y-[-4px]">
+                  <FontAwesomeIcon 
+                  icon={faGithub} 
+                  size="2x" 
+                  className="h-8" />
                 </a>
               </div>
             </div>
