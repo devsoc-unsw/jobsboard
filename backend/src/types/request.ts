d4a0afc35af06195896fce6c7dba33e9676f5525
hanhnguyengiahan <126440514+hanhnguyengiahan@users.noreply.github.com>
2024-07-15 11:56:29 +1000

    Han/add endpoint to delete job on behalf of a company (#478)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Flynn Lambrechts <83333819+flynnlambrechts@users.noreply.github.com>
    Co-authored-by: Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
    Co-authored-by: Jared L <48422312+lhjt@users.noreply.github.com>
    Co-authored-by: Franco Reyes <79299724+francojreyes@users.noreply.github.com>
    Co-authored-by: RichardBao1 <54427922+RichardBao1@users.noreply.github.com>
    Co-authored-by: kevenliu8 <150641727+kevenliu8@users.noreply.github.com>
    Co-authored-by: Keven <kevenliu2807@gmail.com>
    Co-authored-by: Richard  Bao <richardbao@192-168-1-117.tpgi.com.au>
    Co-authored-by: engooo <90817386+engooo@users.noreply.github.com>
    Co-authored-by: brnhan <102663283+brnhan@users.noreply.github.com>
    Co-authored-by: Brian Nhan <brianNhan@student.unsw.edu.au>
    Co-authored-by: hanhnguyengiahan <hanhnguyengiahan@github.com>

--- backend/src/types/request.ts
+++ backend/src/types/request.ts
@@ -55,6 +55,8 @@ export interface AdminCreateJobRequest
 
 export interface AdminApprovedJobPostsRequest extends Request<YearParams>, AdminRequestBase {}
 
+export interface AdminDeleteJobRequest extends Request<JobIDParams, never>, AdminRequestBase {}
+
 // * Student
 export interface StudentPaginatedJobsRequest
   extends Request<PaginatedJobsParams>,
